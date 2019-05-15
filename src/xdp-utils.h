34caabfdbcaaaa84106993b2c2e687f4df472006
Matthias Clasen <mclasen@redhat.com>
2019-05-15 15:09:13 +0000

    Add a utility for rewriting commandlines
    For flatpaks, this prepends "flatpak run", basically.

--- src/xdp-utils.h
+++ src/xdp-utils.h
@@ -63,6 +63,8 @@ gboolean    xdp_app_info_has_network     (XdpAppInfo  *app_info);
 XdpAppInfo *xdp_get_app_info_from_pid    (pid_t        pid,
                                           GError     **error);
 GAppInfo *  xdp_app_info_load_app_info   (XdpAppInfo *app_info);
+char **     xdp_app_info_rewrite_commandline (XdpAppInfo *app_info,
+                                              const char *const *commandline);
 
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(XdpAppInfo, xdp_app_info_unref)
 
