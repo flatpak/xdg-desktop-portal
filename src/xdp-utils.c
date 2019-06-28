56302e44e75935e6e6687a235879abd14afc646e
Christian Kellner <christian@kellner.me>
2019-06-28 18:36:05 +0200

    utils: use namespace info from bwrap, if available
    Newer versions of bubblewrap write the namespace information to the
    bwrapinfo.json. Try reading that information and thus avoiding the
    extra work to look up the pid namespace information via /proc.
    Fallback to this old mechanism if the bwrapinfo.json does not contain
    the information.

--- src/xdp-utils.c
+++ src/xdp-utils.c
@@ -1546,18 +1546,16 @@ map_pids (DIR     *proc,
   return TRUE;
 }
 
-static pid_t
-xdp_app_info_get_child_pid (XdpAppInfo *app_info,
-                            GError    **error)
+static JsonNode *
+xdg_app_info_load_brwap_info (XdpAppInfo *app_info,
+                              GError    **error)
 {
   g_autoptr(JsonParser) parser = NULL;
+  g_autoptr(JsonNode) root = NULL;
   g_autofree char *instance = NULL;
   g_autofree char *data = NULL;
-  JsonNode *root;
-  JsonObject *cpo;
   gsize len;
   char *path;
-  pid_t pid;
 
   g_return_val_if_fail (app_info != NULL, 0);
 
@@ -1582,7 +1580,7 @@ xdp_app_info_get_child_pid (XdpAppInfo *app_info,
       return 0;
     }
 
-  root = json_parser_get_root (parser);
+  root = json_parser_steal_root (parser);
   if (!root)
     {
       g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
@@ -1597,12 +1595,45 @@ xdp_app_info_get_child_pid (XdpAppInfo *app_info,
       return 0;
     }
 
+  return g_steal_pointer (&root);
+}
+
+static ino_t
+xdp_app_info_get_pid_namespace (JsonNode *root,
+                                GError  **error)
+{
+  JsonNode *node;
+  JsonObject *cpo;
+  gint64 nsid;
+
+  /* xdg_app_info_load_brwap_info assures root is of type object */
+  cpo = json_node_get_object (root);
+  node = json_object_get_member (cpo, "pid-namespace");
+
+  if (node == NULL || !JSON_NODE_HOLDS_VALUE (node))
+    {
+      g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_NOT_FOUND,
+                           "pid-namespace missing");
+      return 0;
+    }
+
+  nsid = json_node_get_int (node);
+  return (ino_t) nsid;
+}
+
+static pid_t
+xdp_app_info_get_child_pid (JsonNode *root,
+                            GError  **error)
+{
+  JsonObject *cpo;
+  pid_t pid;
+
   cpo = json_node_get_object (root);
 
   pid = json_object_get_int_member (cpo, "child-pid");
   if (pid == 0)
-    g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
-                 "Could not parse '%s': child-pid missing", path);
+    g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_NOT_FOUND,
+                         "child-pid missing");
 
   return pid;
 }
@@ -1614,6 +1645,7 @@ xdg_app_info_ensure_pidns (XdpAppInfo  *app_info,
                            DIR         *proc,
                            GError     **error)
 {
+  g_autoptr(JsonNode) root = NULL;
   xdp_lockguard GMutex *guard = NULL;
   xdp_autofd int fd = -1;
   pid_t pid;
@@ -1625,7 +1657,22 @@ xdg_app_info_ensure_pidns (XdpAppInfo  *app_info,
   if (app_info->u.flatpak.pidns_id != 0)
     return TRUE;
 
-  pid = xdp_app_info_get_child_pid (app_info, error);
+  root = xdg_app_info_load_brwap_info (app_info, error);
+  if (root == NULL)
+    return FALSE;
+
+  /* newer versions of bubblewrap contain the namespace
+   * information directly, so we don' thave to go via the
+   * child-pid; if this fails, we fallback to the old way */
+  ns = xdp_app_info_get_pid_namespace (root, NULL);
+  if (ns != 0)
+    {
+      g_debug ("Using pid namespace info from bwrap info");
+      app_info->u.flatpak.pidns_id = ns;
+      return TRUE;
+    }
+
+  pid = xdp_app_info_get_child_pid (root, error);
   if (pid == 0)
     return FALSE;
 
