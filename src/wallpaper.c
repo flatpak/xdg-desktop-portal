481253406b805580391e8c00c55d0a964189edba
Felipe Borges <felipeborges@gnome.org>
2019-10-17 15:04:37 +0200

    Add "wallpaper" to the permission store

--- src/wallpaper.c
+++ src/wallpaper.c
@@ -21,14 +21,20 @@
 #include "config.h"
 
 #include <string.h>
+#include <glib/gi18n.h>
 #include <gio/gio.h>
+#include <gio/gdesktopappinfo.h>
 #include <gio/gunixfdlist.h>
 
 #include "wallpaper.h"
+#include "permissions.h"
 #include "request.h"
 #include "xdp-dbus.h"
 #include "xdp-impl-dbus.h"
 #include "xdp-utils.h"
+
+#define PERMISSION_TABLE "wallpaper"
+#define PERMISSION_ID "wallpaper"
 
 typedef struct _Wallpaper Wallpaper;
 typedef struct _WallpaperClass WallpaperClass;
@@ -44,6 +50,7 @@ struct _WallpaperClass
 };
 
 static XdpImplWallpaper *impl;
+static XdpImplAccess *access_impl;
 static Wallpaper *wallpaper;
 
 GType wallpaper_get_type (void) G_GNUC_CONST;
@@ -51,6 +58,82 @@ static void wallpaper_iface_init (XdpWallpaperIface *iface);
 
 G_DEFINE_TYPE_WITH_CODE (Wallpaper, wallpaper, XDP_TYPE_WALLPAPER_SKELETON,
                          G_IMPLEMENT_INTERFACE (XDP_TYPE_WALLPAPER, wallpaper_iface_init));
+
+static gboolean
+get_set_wallpaper_allowed (const gchar *app_id)
+{
+  g_autoptr(GVariant) out_perms = NULL;
+  g_autoptr(GVariant) out_data = NULL;
+  g_autoptr(GError) error = NULL;
+
+  if (!xdp_impl_permission_store_call_lookup_sync (get_permission_store (),
+                                                   PERMISSION_TABLE,
+                                                   PERMISSION_ID,
+                                                   &out_perms,
+                                                   &out_data,
+                                                   NULL,
+                                                   &error))
+    {
+      g_dbus_error_strip_remote_error (error);
+      g_debug ("No wallpaper permissions found: %s", error->message);
+      g_clear_error (&error);
+    }
+
+  if (out_perms != NULL)
+    {
+      const gchar **perms;
+      if (g_variant_lookup (out_perms, app_id, "^a&s", &perms))
+        {
+          g_autofree gchar *a = g_strjoinv (" ", (gchar **)perms);
+
+          g_debug ("Wallpaper permissions for %s: %s", app_id, a);
+
+          return g_strv_contains (perms, "yes");
+        }
+    }
+
+  return FALSE;
+}
+
+static void
+authorize_app (const gchar *app_id)
+{
+  g_autoptr(GError) error = NULL;
+  const char *permissions[2];
+
+  permissions[0] = "yes";
+  permissions[1] = NULL;
+
+  if (!xdp_impl_permission_store_call_set_permission_sync (get_permission_store (),
+                                                          PERMISSION_TABLE,
+                                                          TRUE,
+                                                          PERMISSION_ID,
+                                                          app_id,
+                                                          (const char * const*)permissions,
+                                                          NULL,
+                                                          &error))
+    {
+      g_dbus_error_strip_remote_error (error);
+      g_warning ("Error updating permission store: %s", error->message);
+    }
+}
+
+static void
+send_response (Request *request,
+               guint response)
+{
+  if (request->exported)
+    {
+      GVariantBuilder opt_builder;
+
+      g_debug ("sending response: %d", response);
+      g_variant_builder_init (&opt_builder, G_VARIANT_TYPE_VARDICT);
+      xdp_request_emit_response (XDP_REQUEST (request),
+                                 response,
+                                 g_variant_builder_end (&opt_builder));
+      request_unexport (request);
+    }
+}
 
 static void
 handle_set_wallpaper_uri_done (GObject *source,
@@ -104,6 +187,7 @@ handle_set_wallpaper_in_thread_func (GTask *task,
   GVariantBuilder opt_builder;
   g_autoptr(XdpImplRequest) impl_request = NULL;
   g_autoptr(GVariant) options = NULL;
+  gboolean show_preview = FALSE;
   int fd;
 
   parent_window = ((const char *)g_object_get_data (G_OBJECT (request), "parent-window"));
@@ -112,6 +196,77 @@ handle_set_wallpaper_in_thread_func (GTask *task,
   options = ((GVariant *)g_object_get_data (G_OBJECT (request), "options"));
 
   REQUEST_AUTOLOCK (request);
+
+  if (app_id[0] == '\0' && getenv("TEST_WALLPAPER"))
+    app_id = "org.gnome.PortalTest";
+
+  g_variant_lookup (options, "show-preview", "b", &show_preview);
+  if (!show_preview && !get_set_wallpaper_allowed (app_id))
+    {
+      guint access_response = 2;
+      g_autoptr(GVariant) access_results = NULL;
+      GVariantBuilder access_opt_builder;
+      g_autofree gchar *title = NULL;
+      g_autofree gchar *subtitle = NULL;
+      const gchar *body;
+
+      g_variant_builder_init (&access_opt_builder, G_VARIANT_TYPE_VARDICT);
+      g_variant_builder_add (&access_opt_builder, "{sv}",
+                             "deny_label", g_variant_new_string (_("Deny")));
+      g_variant_builder_add (&access_opt_builder, "{sv}",
+                             "grant_label", g_variant_new_string (_("Allow")));
+      g_variant_builder_add (&access_opt_builder, "{sv}",
+                             "icon", g_variant_new_string ("preferences-desktop-wallpaper-symbolic"));
+
+      if (g_str_equal (app_id, ""))
+        {
+          title = g_strdup (_("Allow Applications to Set Backgrounds?"));
+          subtitle = g_strdup (_("An application is requesting to be able to change the background image."));
+        }
+      else
+        {
+          g_autoptr(GDesktopAppInfo) info = NULL;
+          const gchar *id;
+          const gchar *name;
+
+          id = g_strconcat (app_id, ".desktop", NULL);
+          info = g_desktop_app_info_new (id);
+          name = g_app_info_get_display_name (G_APP_INFO (info));
+
+          title = g_strdup_printf (_("Allow %s to Set Backgrounds?"), name);
+          subtitle = g_strdup_printf (_("%s is requesting to be able to change the background image."), name);
+        }
+
+      body = _("This permission can be changed at any time from the privacy settings.");
+
+      if (!xdp_impl_access_call_access_dialog_sync (access_impl,
+                                                    request->id,
+                                                    app_id,
+                                                    parent_window,
+                                                    title,
+                                                    subtitle,
+                                                    body,
+                                                    g_variant_builder_end (&access_opt_builder),
+                                                    &access_response,
+                                                    &access_results,
+                                                    NULL,
+                                                    &error))
+        {
+          g_warning ("Failed to show access dialog: %s", error->message);
+          return;
+        }
+
+      if (access_response == 0)
+        {
+          authorize_app (app_id);
+        }
+      else
+        {
+          send_response (request, 2);
+
+          return;
+        }
+    }
 
   if (!uri)
     {
@@ -262,6 +417,13 @@ wallpaper_create (GDBusConnection *connection,
 
   g_dbus_proxy_set_default_timeout (G_DBUS_PROXY (impl), G_MAXINT);
   wallpaper = g_object_new (wallpaper_get_type (), NULL);
+
+  access_impl = xdp_impl_access_proxy_new_sync (connection,
+                                                G_DBUS_PROXY_FLAGS_NONE,
+                                                dbus_name_wallpaper,
+                                                DESKTOP_PORTAL_OBJECT_PATH,
+                                                NULL,
+                                                &error);
 
   return G_DBUS_INTERFACE_SKELETON (wallpaper);
 }
