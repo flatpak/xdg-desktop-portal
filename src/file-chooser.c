00b28db5ca45bcbdfea0ddfff8447a3f8836bfd9
Matthias Clasen <mclasen@redhat.com>
2020-03-21 15:11:03 -0400

    Bump the version of the file chooser interface
    This lets clients find out if the directory option
    is supported.

--- src/file-chooser.c
+++ src/file-chooser.c
@@ -721,7 +721,7 @@ file_chooser_iface_init (XdpFileChooserIface *iface)
 static void
 file_chooser_init (FileChooser *fc)
 {
-  xdp_file_chooser_set_version (XDP_FILE_CHOOSER (fc), 2);
+  xdp_file_chooser_set_version (XDP_FILE_CHOOSER (fc), 3);
 }
 
 static void
