eebb04172d004a12412cfb2ba7472c2b67e47b04
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-07-27 20:32:58 +0200

    use correct type for the session object path
    The session id is defined as the object path of the session.
    We should use a type of 'o' instead of 's'.

--- src/remote-desktop.c
+++ src/remote-desktop.c
@@ -221,7 +221,7 @@ create_session_done (GObject *source_object,
     }
 
   g_variant_builder_add (&results_builder, "{sv}",
-                         "session_handle", g_variant_new ("s", session->id));
+                         "session_handle", g_variant_new ("o", session->id));
 
 out:
   if (request->exported)
