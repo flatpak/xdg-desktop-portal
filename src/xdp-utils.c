flatpak/xdg-desktop-portal

commit b19eed79d8f685138ac788d1b158b9e58f9ad4f7

xdp-utils: Avoid custom code and just use GMutexLocker
It's available in glib since longer time than we need.

src/xdp-utils.c
@@ -1922,13 +1922,13 @@ xdg_app_info_ensure_pidns (XdpAppInfo  *app_info,
                            GError     **error)
 {
   g_autoptr(JsonNode) root = NULL;
-  xdp_lockguard GMutex *guard = NULL;
+  g_autoptr(GMutexLocker) guard = NULL;
   xdp_autofd int fd = -1;
   pid_t pid;
   ino_t ns;
   int r;
 
-  guard = xdp_auto_lock_helper (app_info->u.flatpak.pidns_lock);
+  guard = g_mutex_locker_new (app_info->u.flatpak.pidns_lock);
 
   if (app_info->u.flatpak.pidns_id != 0)
     return TRUE;

src/xdp-utils.h
@@ -157,22 +157,10 @@ xdp_close_fd (int *fdp)
 
 #define xdp_autofd __attribute__((cleanup(xdp_close_fd)))
 
-static inline void
-xdp_auto_unlock_helper (GMutex **mutex)
-{
-  if (*mutex)
-    g_mutex_unlock (*mutex);
-}
-
-static inline GMutex *
-xdp_auto_lock_helper (GMutex *mutex)
-{
-  if (mutex)
-    g_mutex_lock (mutex);
-  return mutex;
-}
-
-#define XDP_AUTOLOCK(name) G_GNUC_UNUSED __attribute__((cleanup (xdp_auto_unlock_helper))) GMutex * G_PASTE (auto_unlock, __LINE__) = xdp_auto_lock_helper (&G_LOCK_NAME (name))
+#define XDP_AUTOLOCK(name) \
+  g_autoptr(GMutexLocker) G_PASTE (name ## locker, __LINE__) = \
+    g_mutex_locker_new (&G_LOCK_NAME (name)); \
+  (void) G_PASTE (name ## locker, __LINE__);
 
 
 char *   xdp_quote_argv (const char           *argv[]);

commit 2e5662684059a289064eb013e4212c675095fa39

utils: Remove unused macro definition
Fallout from b19eed79d8f685138ac788d1b158b9e58f9ad4f7

src/xdp-utils.c
@@ -2078,8 +2078,6 @@ xdp_app_info_get_child_pid (JsonNode *root,
   return pid;
 }
 
-#define xdp_lockguard G_GNUC_UNUSED __attribute__((cleanup(xdp_auto_unlock_helper)))
-
 static gboolean
 xdp_app_info_ensure_pidns (XdpAppInfo  *app_info,
                            DIR         *proc,