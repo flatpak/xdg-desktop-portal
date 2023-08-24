1ddab3215e4e0dca1fd6880bcde0f9ad98df634d
Emmanuele Bassi <ebassi@gnome.org>
2023-08-24 17:42:50 +0100

    Port the docs to Sphinx
    Docbook is a barrier to contribution, and the generated output isn't
    the most appealing.
    
    Luckily for us, there are better documentation generators, these days,
    and they don't all use XML; one of these generators is Sphinx, which
    is commonly used by communities like Python, by projects like GCC, the
    Linux kernel, and GNOME. Sphinx uses reStructuredText, which is a
    well-specified plain text format.
    
    The gdbus-codegen tool already supports generating reStructuredText
    from the XML introspection of the D-Bus interfaces.

---
+++ doc/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'XDG Desktop Portals'
+copyright = '2023, XDG Desktop Portal Authors'
+author = 'XDG Desktop Portal Authors'
+release = 'xdg-desktop-portal'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ doc/index.rst
@@ -0,0 +1,31 @@
+.. XDG Desktop Portals documentation master file, created by
+   sphinx-quickstart on Thu Aug 24 16:58:13 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to XDG Desktop Portals's documentation!
+===============================================
+
+.. toctree::
+   :maxdepth: 1
+   :caption: For portal users and integrators
+
+   portals.conf
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: For application developers
+
+   common-conventions
+   portal-interfaces
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: For portal developers
+
+   implementation-interfaces
+
+
+Welcome to the documentation for XDG Desktop Portals.
