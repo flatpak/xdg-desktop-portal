d99796f1be6c8ea7707f9ae5f4400e17994627c0
Peter Hutterer <peter.hutterer@who-t.net>
2023-11-03 14:33:30 +1000

    open-uri: pass an empty string if we don't have an activation token
    If the activation token is NULL, we fail here with
     g_variant_new_string: assertion 'string != NULL' failed
    
    Fixes 69e2faef94f12734706c942ae99248a122fe2f49

--- src/open-uri.c
+++ src/open-uri.c
@@ -741,7 +741,7 @@ handle_open_in_thread_func (GTask *task,
                                                   FILE_MANAGER_DBUS_PATH,
                                                   FILE_MANAGER_DBUS_IFACE,
                                                   FILE_MANAGER_SHOW_ITEMS,
-                                                  g_variant_new ("(ass)", uris_builder, activation_token),
+                                                  g_variant_new ("(ass)", uris_builder, activation_token ? activation_token : ""),
                                                   NULL,   /* ignore returned type */
                                                   G_DBUS_CALL_FLAGS_NONE,
                                                   -1,
