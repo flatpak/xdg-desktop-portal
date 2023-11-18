46268f3c490f6c25cd8a1688f1df62c7dafc9e6f
Hubert Figuière <hub@figuiere.net>
2023-11-17 22:40:43 -0500

    test-document-fuse: Added license header
    Signed-off-by: Hubert Figuière <hub@figuiere.net>

--- tests/test-document-fuse.py
+++ tests/test-document-fuse.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
 
+# Copyright © 2020 Red Hat, Inc
+# Copyright © 2023 GNOME Foundation Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#       Alexander Larsson <alexl@redhat.com>
+#       Hubert Figuière <hub@figuiere.net>
+
 import argparse
 import errno
 import os
