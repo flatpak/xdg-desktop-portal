3cbee2379015ab62629c7c8e13e6149d73e7b84c
Razze <razzeee@gmail.com>
2023-11-22 01:33:21 +0100

    Add opengraph plugin

--- doc/conf.py
+++ doc/conf.py
@@ -6,22 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'XDG Desktop Portals'
-copyright = '2023, XDG Desktop Portal Authors'
-author = 'XDG Desktop Portal Authors'
+project = "XDG Desktop Portals"
+copyright = "2023, XDG Desktop Portal Authors"
+author = "XDG Desktop Portal Authors"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinxext.opengraph",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
+
+# -- Options for OpenGraph ---------------------------------------------------
+
+ogp_site_url = "https://flatpak.github.io/xdg-desktop-portal/docs/"
+ogp_image = "card.png"
