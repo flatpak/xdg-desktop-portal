e598d6c3edeb3697a14ed5c4b1dbc4b88687386f
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2023-12-20 14:32:48 -0300

    impl-request: Do not load properties on construction
    The org.freedesktop.impl.portal.Request interface has no properties,
    and is constructed against a different object path. We don't need to
    fetch properties for that.
    
    Don't do that.
    
    Closes: https://github.com/flatpak/xdg-desktop-portal/issues/1241

--- src/account.c
+++ src/account.c
@@ -189,7 +189,7 @@ handle_get_user_information (XdpDbusAccount *object,
   REQUEST_AUTOLOCK (request);
 
   impl_request = xdp_dbus_impl_request_proxy_new_sync (g_dbus_proxy_get_connection (G_DBUS_PROXY (impl)),
-                                                       G_DBUS_PROXY_FLAGS_NONE,
+                                                       G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES,
                                                        g_dbus_proxy_get_name (G_DBUS_PROXY (impl)),
                                                        request->id,
                                                        NULL, &error);
