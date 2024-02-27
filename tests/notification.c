1285b431fce8ae7329b4f9d79137c725e6ec441f
Julian Sparber <julian@sparber.net>
2024-02-27 15:01:21 +0100

    test: Implement tests for notification icons
    Co-authored-by: Georges Basile Stavracas Neto <feaneron@igalia.com>

--- tests/notification.c
+++ tests/notification.c
@@ -6,6 +6,10 @@
 #include <libportal/portal.h>
 #include "xdp-utils.h"
 
+#define SVG_IMAGE_DATA \
+  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" \
+  "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"16px\" width=\"16px\"/>"
+
 extern char outdir[];
 
 static int got_info;
@@ -25,6 +29,10 @@ notification_succeed (GObject      *source,
   res = xdp_portal_add_notification_finish (portal, result, &error);
   g_assert_no_error (error);
   g_assert_true (res);
+
+  got_info++;
+
+  g_main_context_wakeup (NULL);
 }
 
 static void
@@ -192,4 +200,75 @@ test_notification_bad_button (void)
                    "}";
 
   run_notification_test ("test5", notification_s, NULL, TRUE);
+}
+
+static void
+test_icon (const char *serialized_icon,
+           const char *expected_serialized_icon,
+           gboolean    expect_failure)
+{
+  g_autofree char *expected_notification_s = NULL;
+  g_autofree char *notification_s = NULL;
+
+  notification_s = g_strdup_printf ("{ 'title': <'test notification 7'>, "
+                                    "  'body': <'test notification body 7'>, "
+                                    "  'icon': <%s>, "
+                                    "  'default-action': <'test-action'> "
+                                    "}",
+                                    serialized_icon);
+
+  if (expected_serialized_icon)
+    expected_notification_s = g_strdup_printf ("{ 'title': <'test notification 7'>, "
+                                               "  'body': <'test notification body 7'>, "
+                                               "  'icon': <%s>, "
+                                               "  'default-action': <'test-action'> "
+                                               "}",
+                                               expected_serialized_icon);
+
+  run_notification_test ("test-icon", notification_s, expected_serialized_icon, expect_failure);
+}
+
+static void
+test_themed_icon (void)
+{
+  g_autoptr(GVariant) serialized_icon = NULL;
+  g_autoptr(GIcon) icon = NULL;
+  g_autofree char *serialized_icon_s = NULL;
+
+  icon = g_themed_icon_new ("test-icon-symbolic");
+  serialized_icon = g_icon_serialize (icon);
+
+  serialized_icon_s = g_variant_print (serialized_icon, TRUE);
+  test_icon (serialized_icon_s, NULL, FALSE);
+}
+
+static void
+test_bytes_icon (void)
+{
+  g_autoptr(GVariant) serialized_icon = NULL;
+  g_autoptr(GBytes) bytes = NULL;
+  g_autoptr(GIcon) icon = NULL;
+  g_autofree char *serialized_icon_s = NULL;
+
+  bytes = g_bytes_new_static (SVG_IMAGE_DATA, strlen (SVG_IMAGE_DATA));
+  icon = g_bytes_icon_new (bytes);
+  serialized_icon = g_icon_serialize (icon);
+
+  serialized_icon_s = g_variant_print (serialized_icon, TRUE);
+  test_icon (serialized_icon_s, NULL, FALSE);
+}
+
+void
+test_notification_icon (void)
+{
+  /* For historical reasons we also accept just an icon name but it's
+   * converted to a "normal" themed icon */
+  test_icon ("'test-icon'", "('themed', <['test-icon', 'test-icon-symbolic']>)", FALSE);
+
+  test_themed_icon ();
+  test_bytes_icon ();
+
+  /* Tests that should fail */
+  test_icon ("('themed', <'test-icon-symbolic'>)", NULL, TRUE);
+  test_icon ("('bytes', <['test-icon-symbolic', 'test-icon']>)", NULL, TRUE);
 }
