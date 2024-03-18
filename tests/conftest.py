486468e3e4de00ab9e1fa957319a535deab6a427
Sebastian Wick <sebastian.wick@redhat.com>
2024-03-18 21:31:08 +0100

    tests: Force a default app id for python tests
    Instead of querying x-d-p about the app id, we force the app id that
    x-d-p uses for all client connections.
    
    The `app_id` fixture and PortalMock allow to override this value. The
    `app_id` fixture can be used in tests to access the app_id that x-d-p
    will see.

--- tests/conftest.py
+++ tests/conftest.py
@@ -57,11 +57,19 @@ def params() -> dict[str, Any]:
 
 
 @pytest.fixture
-def portal_mock(session_bus, portal_name, params, portal_has_impl) -> PortalMock:
+def app_id():
+    """
+    Default fixture providing the app id of the connecting process
+    """
+    return "org.example.App"
+
+
+@pytest.fixture
+def portal_mock(session_bus, portal_name, params, portal_has_impl, app_id) -> PortalMock:
     """
     Fixture yielding a PortalMock object with the impl started, if applicable.
     """
-    pmock = PortalMock(session_bus, portal_name)
+    pmock = PortalMock(session_bus, portal_name, app_id)
     if portal_has_impl:
         pmock.start_impl_portal(params)
     pmock.start_xdp()
