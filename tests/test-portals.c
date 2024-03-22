da989a577821d73833c5d12fcde849743e3ab7db
Julian Sparber <julian@sparber.net>
2024-03-22 17:25:12 +0100

    test: Implement tests for display-hint notification property

--- tests/test-portals.c
+++ tests/test-portals.c
@@ -609,6 +609,7 @@ main (int argc, char **argv)
   g_test_add_func ("/portal/notification/icon", test_notification_icon);
   g_test_add_func ("/portal/notification/sound", test_notification_sound);
   g_test_add_func ("/portal/notification/desktop-file-id", test_notification_desktop_file_id);
+  g_test_add_func ("/portal/notification/display-hint", test_notification_display_hint);
 #endif
 
   global_setup ();
