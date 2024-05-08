flatpak/xdg-desktop-portal

commit ede383e72cd24b609a4b8c271cfd367869474ed0

document-portal: Use libfuse3 API
Use the "new" fuse API that is becoming the default given that libfuse2
is not maintained anymore.

No major logic changes are done here, and all is preserved as it used to
be, but we ensure that the fuse session is created and destroyed in the
same thread.
Also, we require capabilities through the init vfunc, as some features
can't be requested anymore via fuse_args.

Fixes: #194

document-portal/document-portal-fuse.c
@@ -1,6 +1,6 @@
 #include "config.h"
 
-#define FUSE_USE_VERSION 26
+#define FUSE_USE_VERSION 35
 
 #include <glib-unix.h>
 
@@ -88,7 +88,6 @@
 
 static GThread *fuse_thread = NULL;
 static struct fuse_session *session = NULL;
-static struct fuse_chan *main_ch = NULL;
 static char *mount_path = NULL;
 static pthread_t fuse_pthread = 0;
 static uid_t my_uid;
@@ -1668,8 +1667,9 @@ invalidate_doc_domain (gpointer user_data)
 
   parent_ino = xdp_inode_to_ino (doc_domain->parent_inode);
 
-  if (g_atomic_int_get (&doc_domain->parent_inode->kernel_ref_count) > 0 && main_ch != NULL)
-    fuse_lowlevel_notify_inval_entry (main_ch, parent_ino, doc_domain->doc_id, strlen (doc_domain->doc_id));
+  if (g_atomic_int_get (&doc_domain->parent_inode->kernel_ref_count) > 0 &&
+      g_atomic_pointer_get (&session) != NULL)
+    fuse_lowlevel_notify_inval_entry (session, parent_ino, doc_domain->doc_id, strlen (doc_domain->doc_id));
 
   return FALSE;
 }
@@ -2465,7 +2465,8 @@ xdp_fuse_rename (fuse_req_t  req,
                  fuse_ino_t  parent_ino,
                  const char *name,
                  fuse_ino_t  newparent_ino,
-                 const char *newname)
+                 const char *newname,
+                 unsigned int flags)
 {
   g_autoptr(XdpInode) parent = xdp_inode_from_ino (parent_ino);
   g_autoptr(XdpInode) newparent = xdp_inode_from_ino (newparent_ino);
@@ -3004,20 +3005,26 @@ xdp_fuse_init_cb (void                  *userdata,
                   struct fuse_conn_info *conn)
 {
   g_debug ("INIT");
+
+  /* splice_read: use splice() to read from fuse pipe */
+  conn->want |= FUSE_CAP_SPLICE_READ;
+  /* splice_write: use splice() to write to fuse pipe */
+  conn->want |= FUSE_CAP_SPLICE_WRITE;
+  /* splice_move: move buffers from writing app to kernel during splice write */
+  conn->want |= FUSE_CAP_SPLICE_MOVE;
+  /* atomic_o_trunc: We handle O_TRUNC in create() */
+  conn->want |= FUSE_CAP_ATOMIC_O_TRUNC;
 }
 
-extern void on_fuse_unmount (void);
-
-static int destroyed;
+extern int on_fuse_unmount (void);
 
 static void
 xdp_fuse_destroy_cb (void *userdata)
 {
   g_debug ("DESTROY");
 
-  destroyed = 1;
-
-  on_fuse_unmount ();
+  /* Ensure we call this on the main thread */
+  g_idle_add ((GSourceFunc) on_fuse_unmount, NULL);
 }
 
 static struct fuse_lowlevel_ops xdp_fuse_oper = {
@@ -3059,14 +3066,19 @@ static struct fuse_lowlevel_ops xdp_fuse_oper = {
  .fallocate    = xdp_fuse_fallocate,
 };
 
-static gpointer
-xdp_fuse_mainloop (gpointer data)
+typedef struct {
+  GMutex lock;
+  GCond cond;
+  GError *error;
+} XdpFuseThreadData;
+
+static void
+xdp_fuse_mainloop (struct fuse_session *se,
+                   struct fuse_loop_config *loop_config)
 {
   const char *status;
 
-  fuse_pthread = pthread_self ();
-
-  fuse_session_loop_mt (session);
+  fuse_session_loop_mt (se, loop_config);
 
   status = getenv ("TEST_DOCUMENT_PORTAL_FUSE_STATUS");
   if (status)
@@ -3079,11 +3091,73 @@ xdp_fuse_mainloop (gpointer data)
       g_file_set_contents (status, s->str, -1, &error);
       g_assert_no_error (error);
     }
+}
 
-  fuse_session_remove_chan (main_ch);
-  fuse_session_destroy (session);
-  fuse_unmount (mount_path, main_ch);
-  main_ch = NULL;
+typedef struct fuse_args XdpAutoFuseArgs;
+G_DEFINE_AUTO_CLEANUP_CLEAR_FUNC (XdpAutoFuseArgs, fuse_opt_free_args);
+
+static gpointer
+xdp_fuse_thread (gpointer data)
+{
+  /* Options:
+   *  auto_unmount: Tell fusermount to auto unmount if we die.
+   */
+  static char *fusermount_argv[] = {
+    "xdp-fuse", "-osubtype=portal,fsname=portal,auto_unmount",
+  };
+  g_auto(XdpAutoFuseArgs) args =
+    FUSE_ARGS_INIT (G_N_ELEMENTS (fusermount_argv), fusermount_argv);
+  g_autoptr(GMutexLocker) locker = NULL;
+  const char *path;
+  struct fuse_session *se;
+  XdpFuseThreadData *thread_data = data;
+  struct fuse_cmdline_opts opts = {0};
+  struct fuse_loop_config loop_config = {0};
+
+  locker = g_mutex_locker_new (&thread_data->lock);
+  fuse_pthread = pthread_self ();
+
+  g_cond_signal (&thread_data->cond);
+
+  if (fuse_parse_cmdline (&args, &opts) != 0)
+    {
+      g_set_error (&thread_data->error, XDG_DESKTOP_PORTAL_ERROR,
+                   XDG_DESKTOP_PORTAL_ERROR_INVALID_ARGUMENT,
+                   "Impossible to parse command line");
+      return NULL;
+    }
+
+  se = fuse_session_new (&args, &xdp_fuse_oper,
+                         sizeof (xdp_fuse_oper), NULL);
+  if (se == NULL)
+    {
+      g_set_error (&thread_data->error, XDG_DESKTOP_PORTAL_ERROR,
+                   XDG_DESKTOP_PORTAL_ERROR_FAILED,
+                   "Can't create fuse session");
+      return NULL;
+    }
+
+  path = xdp_fuse_get_mountpoint ();
+  if (fuse_session_mount (se, path) != 0)
+    {
+      fuse_session_destroy (se);
+      g_set_error (&thread_data->error, XDG_DESKTOP_PORTAL_ERROR,
+                   XDG_DESKTOP_PORTAL_ERROR_FAILED,
+                   "Can't mount path %s", path);
+      return NULL;
+    }
+
+  session = se;
+  thread_data = NULL;
+  g_clear_pointer (&locker, g_mutex_locker_free);
+
+  loop_config.clone_fd = opts.clone_fd;
+  loop_config.max_idle_threads = opts.max_idle_threads;
+  xdp_fuse_mainloop (session, &loop_config);
+
+  fuse_session_unmount (se);
+  g_atomic_pointer_set (&session, NULL);
+  fuse_session_destroy (se);
 
   return NULL;
 }
@@ -3091,16 +3165,7 @@ xdp_fuse_mainloop (gpointer data)
 gboolean
 xdp_fuse_init (GError **error)
 {
-  /* Options:
-   *  auto_unmount: Tell fusermount to auto unmount if we die.
-   *  splice_read: use splice() to read from fuse pipe
-   *  splice_write: use splice() to write to fuse pipe
-   *  splice_move: move buffers from writing app to kernel during splice write
-   *  atomic_o_trunc: We handle O_TRUNC in create()
-   *  big_writes: Allow > 4k writes
-   */
-  char *fusermount_argv[] = { "xdp-fuse", "-osubtype=portal,fsname=portal,auto_unmount,splice_read,splice_write,splice_move,atomic_o_trunc,big_writes" };
-  struct fuse_args args = FUSE_ARGS_INIT (G_N_ELEMENTS (fusermount_argv), fusermount_argv);
+  XdpFuseThreadData thread_data = {0};
   struct stat st;
   struct statfs stfs;
   const char *path;
@@ -3137,7 +3202,7 @@ xdp_fuse_init (GError **error)
        (statfs_res == 0 && stfs.f_type == 0x65735546 /* fuse */)))
     {
       int count;
-      char *umount_argv[] = { "fusermount", "-u", "-z", (char *) path, NULL };
+      char *umount_argv[] = { "fusermount3", "-u", "-z", (char *) path, NULL };
 
       g_spawn_sync (NULL, umount_argv, NULL, G_SPAWN_SEARCH_PATH,
                     NULL, NULL, NULL, NULL, NULL, NULL);
@@ -3155,29 +3220,26 @@ xdp_fuse_init (GError **error)
       return FALSE;
     }
 
-  main_ch = fuse_mount (path, &args);
-  if (main_ch == NULL)
+  g_mutex_init (&thread_data.lock);
+  g_cond_init (&thread_data.cond);
+
+  g_mutex_lock (&thread_data.lock);
+  fuse_thread = g_thread_new ("fuse mainloop", xdp_fuse_thread, &thread_data);
+
+  while (session == NULL && thread_data.error == NULL)
+    g_cond_wait (&thread_data.cond, &thread_data.lock);
+
+  g_mutex_unlock (&thread_data.lock);
+  g_cond_clear (&thread_data.cond);
+  g_mutex_clear (&thread_data.lock);
+
+  if (thread_data.error != NULL)
     {
-      fuse_opt_free_args (&args);
-      g_set_error (error, XDG_DESKTOP_PORTAL_ERROR, XDG_DESKTOP_PORTAL_ERROR_FAILED,
-                   "Can't mount fuse fs on %s: %s", path, g_strerror (errno));
+      g_propagate_error (error, g_steal_pointer (&thread_data.error));
       return FALSE;
     }
 
-  session = fuse_lowlevel_new (&args, &xdp_fuse_oper,
-                               sizeof (xdp_fuse_oper), NULL);
-  if (session == NULL)
-    {
-      fuse_opt_free_args (&args);
-      g_set_error (error, XDG_DESKTOP_PORTAL_ERROR, XDG_DESKTOP_PORTAL_ERROR_FAILED,
-                   "Can't create fuse session");
-      return FALSE;
-    }
-  fuse_session_add_chan (session, main_ch);
-
-  fuse_thread = g_thread_new ("fuse mainloop", xdp_fuse_mainloop, session);
-
-  fuse_opt_free_args (&args);
+  g_assert (session != NULL);
 
   return TRUE;
 }
@@ -3185,14 +3247,13 @@ xdp_fuse_init (GError **error)
 void
 xdp_fuse_exit (void)
 {
-  if (!destroyed && session)
+  if (g_atomic_pointer_get (&session))
     fuse_session_exit (session);
 
   if (fuse_pthread)
     pthread_kill (fuse_pthread, SIGHUP);
 
-  if (fuse_thread)
-    g_thread_join (fuse_thread);
+  g_clear_pointer (&fuse_thread, g_thread_join);
 }
 
 const char *
@@ -3243,7 +3304,7 @@ xdp_fuse_invalidate_doc_app (const char *doc_id,
 
   /* This can happen if fuse is not initialized yet for the very
      first dbus message that activated the service */
-  if (main_ch == NULL)
+  if (g_atomic_pointer_get (&session) == NULL)
     return;
 
   g_debug ("invalidate %s/%s", doc_id, opt_app_id ? opt_app_id : "*");
@@ -3276,12 +3337,12 @@ xdp_fuse_invalidate_doc_app (const char *doc_id,
 
       if (invalidate->filename)
         {
-          fuse_lowlevel_notify_inval_entry (main_ch, invalidate->ino,
+          fuse_lowlevel_notify_inval_entry (session, invalidate->ino,
                                             invalidate->filename, strlen (invalidate->filename));
           g_free (invalidate->filename);
         }
       else
-        fuse_lowlevel_notify_inval_inode (main_ch, invalidate->ino, 0, 0);
+        fuse_lowlevel_notify_inval_inode (session, invalidate->ino, 0, 0);
     }
 }
 

.github/workflows/check.yml
@@ -59,8 +59,9 @@ jobs:
         $RUN_CMD apt-get upgrade -y
         $RUN_CMD apt-get install -y --no-install-recommends \
           make automake autoconf libtool gettext autopoint ${{ matrix.compiler }} \
-          gtk-doc-tools shared-mime-info desktop-file-utils gnome-desktop-testing fuse libflatpak-dev \
-          libglib2.0-dev libgeoclue-2-dev libjson-glib-dev libfontconfig1-dev libfuse-dev libportal-dev libpipewire-0.3-dev
+          gtk-doc-tools shared-mime-info desktop-file-utils gnome-desktop-testing \
+          fuse3 libflatpak-dev libglib2.0-dev libgeoclue-2-dev libjson-glib-dev \
+          libfontconfig1-dev libfuse3-dev libportal-dev libpipewire-0.3-dev
 
     - name: Check out xdg-desktop-portal
       uses: actions/checkout@v2

.github/workflows/docs.yml
@@ -16,8 +16,9 @@ jobs:
           apt-get upgrade -y
           apt-get install -y --no-install-recommends \
           make automake autoconf libtool gettext autopoint gcc git ca-certificates \
-          gtk-doc-tools shared-mime-info desktop-file-utils gnome-desktop-testing xmlto fuse libflatpak-dev \
-          libglib2.0-dev libgeoclue-2-dev libjson-glib-dev libfontconfig1-dev libfuse-dev libportal-dev libpipewire-0.3-dev
+          gtk-doc-tools shared-mime-info desktop-file-utils gnome-desktop-testing \
+          xmlto fuse3 libflatpak-dev libglib2.0-dev libgeoclue-2-dev libjson-glib-dev \
+          libfontconfig1-dev libfuse3-dev libportal-dev libpipewire-0.3-dev
 
       - name: Check out xdg-desktop-portal
         uses: actions/checkout@v2

configure.ac
@@ -171,7 +171,7 @@ fi
 
 GLIB_TESTS
 
-PKG_CHECK_MODULES(FUSE, [fuse])
+PKG_CHECK_MODULES(FUSE3, [fuse3 >= 3.10.0])
 
 AC_CONFIG_FILES([
 Makefile

document-portal/Makefile.am.inc
@@ -100,5 +100,5 @@ xdg_document_portal_SOURCES = \
 	$(DB_SOURCES) \
 	$(NULL)
 
-xdg_document_portal_LDADD = $(AM_LDADD) $(BASE_LIBS) $(FUSE_LIBS)
-xdg_document_portal_CFLAGS = $(AM_CFLAGS) $(BASE_CFLAGS)  $(FUSE_CFLAGS) -I$(srcdir)/document-portal -I$(builddir)/document-portal
+xdg_document_portal_LDADD = $(AM_LDADD) $(BASE_LIBS) $(FUSE3_LIBS)
+xdg_document_portal_CFLAGS = $(AM_CFLAGS) $(BASE_CFLAGS)  $(FUSE3_CFLAGS) -I$(srcdir)/document-portal -I$(builddir)/document-portal

document-portal/document-portal.c
@@ -1501,11 +1501,11 @@ on_name_lost (GDBusConnection *connection,
   g_main_loop_quit (loop);
 }
 
-void
+int
 on_fuse_unmount (void)
 {
   if (!g_main_loop_is_running (loop))
-    return;
+    return G_SOURCE_REMOVE;
 
   g_debug ("fuse fs unmounted externally");
 
@@ -1516,6 +1516,8 @@ on_fuse_unmount (void)
     g_set_error (&exit_error, G_IO_ERROR, G_IO_ERROR_FAILED, "Fuse filesystem unmounted");
 
   g_main_loop_quit (loop);
+
+  return G_SOURCE_REMOVE;
 }
 
 static void

tests/Makefile.am.inc
@@ -13,11 +13,11 @@ testdb_LDADD = \
 	$(NULL)
 testdb_SOURCES = tests/testdb.c	$(DB_SOURCES)
 
-test_doc_portal_CFLAGS = $(AM_CFLAGS) $(BASE_CFLAGS) $(FUSE_CFLAGS)
+test_doc_portal_CFLAGS = $(AM_CFLAGS) $(BASE_CFLAGS) $(FUSE3_CFLAGS)
 test_doc_portal_LDADD = \
 	$(AM_LDADD) \
 	$(BASE_LIBS) \
-	$(FUSE_LIBS) \
+	$(FUSE3_LIBS) \
 	$(NULL)
 test_doc_portal_SOURCES = \
 	tests/can-use-fuse.c \

tests/can-use-fuse.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2019-2021 Collabora Ltd.
+ * Copyright 2021 Canonical Ltd.
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
@@ -12,7 +13,7 @@
 
 #include <glib/gstdio.h>
 
-#define FUSE_USE_VERSION 26
+#define FUSE_USE_VERSION 35
 #include <fuse_lowlevel.h>
 
 gchar *cannot_use_fuse = NULL;
@@ -26,8 +27,9 @@ check_fuse (void)
   g_autofree gchar *fusermount = NULL;
   g_autofree gchar *path = NULL;
   char *argv[] = { "flatpak-fuse-test" };
+  const struct fuse_lowlevel_ops pc_oper = { 0 };
   struct fuse_args args = FUSE_ARGS_INIT (G_N_ELEMENTS (argv), argv);
-  struct fuse_chan *chan = NULL;
+  struct fuse_session *session = NULL;
   g_autoptr(GError) error = NULL;
 
   if (cannot_use_fuse != NULL)
@@ -40,11 +42,11 @@ check_fuse (void)
       return FALSE;
     }
 
-  fusermount = g_find_program_in_path ("fusermount");
+  fusermount = g_find_program_in_path ("fusermount3");
 
   if (fusermount == NULL)
     {
-      cannot_use_fuse = g_strdup ("fusermount not found in PATH");
+      cannot_use_fuse = g_strdup ("fusermount3 not found in PATH");
       return FALSE;
     }
 
@@ -56,16 +58,16 @@ check_fuse (void)
 
   if (!g_file_test ("/etc/mtab", G_FILE_TEST_EXISTS))
     {
-      cannot_use_fuse = g_strdup ("fusermount won't work without /etc/mtab");
+      cannot_use_fuse = g_strdup ("fusermount3 won't work without /etc/mtab");
       return FALSE;
     }
 
   path = g_dir_make_tmp ("flatpak-test.XXXXXX", &error);
   g_assert_no_error (error);
 
-  chan = fuse_mount (path, &args);
+  session = fuse_session_new (&args, &pc_oper, sizeof (pc_oper), &session);
 
-  if (chan == NULL)
+  if (session == NULL)
     {
       fuse_opt_free_args (&args);
       cannot_use_fuse = g_strdup_printf ("fuse_mount: %s",
@@ -73,14 +75,25 @@ check_fuse (void)
       return FALSE;
     }
 
+  if (fuse_session_mount (session, path) != 0)
+    {
+      fuse_opt_free_args (&args);
+      fuse_session_destroy (session);
+      cannot_use_fuse = g_strdup_printf ("fuse_mount: impossible to mount path "
+                                         "'%s': %s",
+                                         path, g_strerror (errno));
+      return FALSE;
+    }
+
   g_test_message ("Successfully set up test FUSE fs on %s", path);
 
-  fuse_unmount (path, chan);
+  fuse_session_unmount (session);
 
   if (g_rmdir (path) != 0)
     g_error ("rmdir %s: %s", path, g_strerror (errno));
 
   fuse_opt_free_args (&args);
+  fuse_session_destroy (session);
 
   return TRUE;
 }

commit 4a3714542acdead0eb546d86e1fb7b7d840d9eda

document-portal: Use portable formatting specifiers in debug strings
%lx was the correct, portable specifier for FUSE 2, where `fuse_ino_t`
was declared as an unsigned long, but isn't correct any more for FUSE 3,
which declares the inode number to be a 64-bit unsigned integer.

This fixes crashes when running the xdg-document-portal binary on 32 bit
architectures.

Relatedly, also use proper format specifiers for `size_t` variables, and
cast `off_t` to `goffset` in print statements, to use G_GOFFSET_FORMAT
on them.

Fixes: ede383e7 "document-portal: Use libfuse3 API".

Signed-off-by: Alberto Mardegan <a.mardegan@omp.ru>

document-portal/document-portal-fuse.c
@@ -1428,7 +1428,7 @@ xdp_fuse_getattr (fuse_req_t req,
   double attr_valid_time = 0.0;/* Time in secs for attribute validation */
   const char *op = "GETATTR";
 
-  g_debug ("GETATTR %lx", ino);
+  g_debug ("GETATTR %" G_GINT64_MODIFIER "x", ino);
 
   if (xdp_domain_is_virtual_type (domain))
     {
@@ -1468,7 +1468,7 @@ xdp_fuse_setattr (fuse_req_t             req,
   int res;
   const char *op = "SETATTR";
 
-  g_debug ("SETATTR %lx %s", ino, to_set_string);
+  g_debug ("SETATTR %" G_GINT64_MODIFIER "x %s", ino, to_set_string);
 
   if (!xdp_document_inode_checks (op, req, inode,
                                   CHECK_CAN_WRITE | CHECK_IS_PHYSICAL))
@@ -1810,7 +1810,7 @@ xdp_fuse_lookup (fuse_req_t req,
   int open_flags = O_PATH|O_NOFOLLOW;
   const char *op = "LOOKUP";
 
-  g_debug ("LOOKUP %lx:%s", parent_ino, name);
+  g_debug ("LOOKUP %" G_GINT64_MODIFIER "x:%s", parent_ino, name);
 
   if (strcmp (name, ".") == 0 || strcmp (name, "..") == 0)
     {
@@ -1860,7 +1860,7 @@ xdp_fuse_lookup (fuse_req_t req,
       queue_invalidate_dentry (parent, name);
     }
 
-  g_debug ("LOOKUP %lx:%s => %lx", parent_ino, name, e.ino);
+  g_debug ("LOOKUP %" G_GINT64_MODIFIER "x:%s => %" G_GINT64_MODIFIER "x", parent_ino, name, e.ino);
 
   if (fuse_reply_entry (req, &e) == -ENOENT)
     abort_reply_entry (&e);
@@ -1895,7 +1895,7 @@ xdp_fuse_open (fuse_req_t req,
   XdpDocumentChecks checks;
   const char *op = "OPEN";
 
-  g_debug ("OPEN %lx %s", ino, open_flags_string);
+  g_debug ("OPEN %" G_GINT64_MODIFIER "x %s", ino, open_flags_string);
 
   checks = CHECK_IS_PHYSICAL;
   if (open_flags_has_write (open_flags))
@@ -1961,7 +1961,7 @@ xdp_fuse_create (fuse_req_t             req,
   XdpFile *file = NULL;
   const char *op = "CREATE";
 
-  g_debug ("CREATE %lx %s %s, 0%o", parent_ino, filename, open_flags_string, mode);
+  g_debug ("CREATE %" G_GINT64_MODIFIER "x %s %s, 0%o", parent_ino, filename, open_flags_string, mode);
 
   if (!xdp_document_inode_checks (op, req, parent,
                                   CHECK_CAN_WRITE |
@@ -2004,7 +2004,7 @@ xdp_fuse_read (fuse_req_t req,
   struct fuse_bufvec buf = FUSE_BUFVEC_INIT(size);
   XdpFile *file = (XdpFile *)fi->fh;
 
-  g_debug ("READ %lx size %ld off %ld", ino, size, off);
+  g_debug ("READ %" G_GINT64_MODIFIER "x size %" G_GSIZE_FORMAT " off %" G_GOFFSET_FORMAT, ino, size, (goffset)off);
 
   buf.buf[0].flags = FUSE_BUF_IS_FD | FUSE_BUF_FD_SEEK;
   buf.buf[0].fd = file->fd;
@@ -2026,7 +2026,7 @@ xdp_fuse_write (fuse_req_t             req,
   ssize_t res;
   const char *op = "WRITE";
 
-  g_debug ("WRITE %lx size %ld off %ld", ino, size, off);
+  g_debug ("WRITE %" G_GINT64_MODIFIER "x size %" G_GSIZE_FORMAT " off %" G_GOFFSET_FORMAT, ino, size, (goffset)off);
 
   res = pwrite (file->fd, buf, size, off);
 
@@ -2048,7 +2048,7 @@ xdp_fuse_write_buf (fuse_req_t             req,
   ssize_t res;
   const char *op = "WRITEBUF";
 
-  g_debug ("WRITEBUF %lx off %ld", ino, off);
+  g_debug ("WRITEBUF %" G_GINT64_MODIFIER "x off %" G_GOFFSET_FORMAT, ino, (goffset)off);
 
   dst.buf[0].flags = FUSE_BUF_IS_FD | FUSE_BUF_FD_SEEK;
   dst.buf[0].fd = file->fd;
@@ -2071,7 +2071,7 @@ xdp_fuse_fsync (fuse_req_t             req,
   int res;
   const char *op = "FSYNC";
 
-  g_debug ("FSYNC %lx", ino);
+  g_debug ("FSYNC %" G_GINT64_MODIFIER "x", ino);
 
   if (datasync)
     res = fdatasync (file->fd);
@@ -2095,7 +2095,7 @@ xdp_fuse_fallocate (fuse_req_t req,
   int res;
   const char *op = "FALLOCATE";
 
-  g_debug ("FALLOCATE %lx", ino);
+  g_debug ("FALLOCATE %" G_GINT64_MODIFIER "x", ino);
 
 #ifdef __linux__
   res = fallocate (file->fd, mode, offset, length);
@@ -2116,7 +2116,7 @@ xdp_fuse_flush (fuse_req_t req,
 {
   const char *op = "FLUSH";
 
-  g_debug ("FLUSH %lx", ino);
+  g_debug ("FLUSH %" G_GINT64_MODIFIER "x", ino);
   xdp_reply_ok (op, req);
 }
 
@@ -2128,7 +2128,7 @@ xdp_fuse_release (fuse_req_t             req,
   XdpFile *file = (XdpFile *)fi->fh;
   const char *op = "RELEASE";
 
-  g_debug ("RELEASE %lx", ino);
+  g_debug ("RELEASE %" G_GINT64_MODIFIER "x", ino);
 
   xdp_file_free (file);
 
@@ -2141,7 +2141,7 @@ forget_one (fuse_ino_t ino,
 {
   g_autoptr(XdpInode) inode = xdp_inode_from_ino (ino);
 
-  g_debug ("FORGET %lx %ld", ino, nlookup);
+  g_debug ("FORGET %" G_GINT64_MODIFIER "x %ld", ino, nlookup);
   xdp_inode_kernel_unref (inode, nlookup);
 }
 
@@ -2161,7 +2161,7 @@ xdp_fuse_forget_multi (fuse_req_t req,
 {
   size_t i;
 
-  g_debug ("FORGET_MULTI %ld", count);
+  g_debug ("FORGET_MULTI %" G_GSIZE_FORMAT, count);
 
   for (i = 0; i < count; i++)
     forget_one (forgets[i].ino, forgets[i].nlookup);
@@ -2278,7 +2278,7 @@ xdp_fuse_opendir (fuse_req_t             req,
   DIR *dir;
   const char *op = "OPENDIR";
 
-  g_debug ("OPENDIR %lx domain %d", ino, inode->domain->type);
+  g_debug ("OPENDIR %" G_GINT64_MODIFIER "x domain %d", ino, inode->domain->type);
 
   if (xdp_domain_is_virtual_type (domain))
     {
@@ -2380,7 +2380,7 @@ xdp_fuse_readdir (fuse_req_t req,
   size_t rem;
   const char *op = "READDIR";
 
-  g_debug ("READDIR %lx %ld %ld", ino, size, off);
+  g_debug ("READDIR %" G_GINT64_MODIFIER "x %" G_GSIZE_FORMAT " %" G_GOFFSET_FORMAT, ino, size, (goffset)off);
 
   if (d->dir)
     {
@@ -2467,7 +2467,7 @@ xdp_fuse_releasedir (fuse_req_t             req,
   XdpDir *d = (XdpDir *)fi->fh;
   const char *op = "RELEASEDIR";
 
-  g_debug ("RELEASEDIR %lx", ino);
+  g_debug ("RELEASEDIR %" G_GINT64_MODIFIER "x", ino);
 
   xdp_dir_free (d);
 
@@ -2484,7 +2484,7 @@ xdp_fuse_fsyncdir (fuse_req_t             req,
   int fd, res;
   const char *op = "FSYNCDIR";
 
-  g_debug ("FSYNCDIR %lx", ino);
+  g_debug ("FSYNCDIR %" G_GINT64_MODIFIER "x", ino);
 
   if (dir->dir)
     {
@@ -2516,7 +2516,7 @@ xdp_fuse_mkdir (fuse_req_t  req,
   int dirfd;
   const char *op = "MKDIR";
 
-  g_debug ("MKDIR %lx %s", parent_ino, name);
+  g_debug ("MKDIR %" G_GINT64_MODIFIER "x %s", parent_ino, name);
 
   if (!xdp_document_inode_checks (op, req, parent,
                                   CHECK_CAN_WRITE |
@@ -2550,7 +2550,7 @@ xdp_fuse_unlink (fuse_req_t  req,
   int res = -1;
   const char * op = "UNLINK";
 
-  g_debug ("UNLINK %lx %s", parent_ino, filename);
+  g_debug ("UNLINK %" G_GINT64_MODIFIER "x %s", parent_ino, filename);
 
   if (!xdp_document_inode_checks (op, req, parent,
                                   CHECK_CAN_WRITE |
@@ -2636,7 +2636,7 @@ xdp_fuse_rename (fuse_req_t  req,
   xdp_autofd int close_fd2 = -1;
   const char *op = "RENAME";
 
-  g_debug ("RENAME %lx %s -> %lx %s (flags: %s)", parent_ino, name,
+  g_debug ("RENAME %" G_GINT64_MODIFIER "x %s -> %" G_GINT64_MODIFIER "x %s (flags: %s)", parent_ino, name,
            newparent_ino, newname, rename_flags_string);
 
   if (!xdp_document_inode_checks (op, req, parent,
@@ -2790,7 +2790,7 @@ xdp_fuse_access (fuse_req_t req,
   int res;
   const char *op = "ACCESS";
 
-  g_debug ("ACCESS %lx", ino);
+  g_debug ("ACCESS %" G_GINT64_MODIFIER "x", ino);
 
   if (inode->domain->type != XDP_DOMAIN_DOCUMENT)
     {
@@ -2839,7 +2839,7 @@ xdp_fuse_rmdir (fuse_req_t req,
   int res;
   const char *op = "RMDIR";
 
-  g_debug ("RMDIR %lx %s", parent_ino, filename);
+  g_debug ("RMDIR %" G_GINT64_MODIFIER "x %s", parent_ino, filename);
 
   if (!xdp_document_inode_checks (op, req, parent,
                                   CHECK_CAN_WRITE |
@@ -2867,7 +2867,7 @@ xdp_fuse_readlink (fuse_req_t req,
   ssize_t res;
   const char *op = "READLINK";
 
-  g_debug ("READLINK %lx", ino);
+  g_debug ("READLINK %" G_GINT64_MODIFIER "x", ino);
 
   if (!xdp_document_inode_checks (op, req, inode,
                                   CHECK_IS_DIRECTORY |
@@ -2898,7 +2898,7 @@ xdp_fuse_symlink (fuse_req_t req,
   struct fuse_entry_param e;
   const char * op = "SYMLINK";
 
-  g_debug ("SYMLINK %s %lx %s", link, parent_ino, name);
+  g_debug ("SYMLINK %s %" G_GINT64_MODIFIER "x %s", link, parent_ino, name);
 
   if (!xdp_document_inode_checks (op, req, parent,
                                   CHECK_CAN_WRITE |
@@ -2937,7 +2937,7 @@ xdp_fuse_link (fuse_req_t req,
   struct fuse_entry_param e;
   const char * op = "LINK";
 
-  g_debug ("LINK %lx %lx %s", ino, newparent_ino, newname);
+  g_debug ("LINK %" G_GINT64_MODIFIER "x %" G_GINT64_MODIFIER "x %s", ino, newparent_ino, newname);
 
   /* hardlinks only supported in docdirs, and only physical files */
   if (!xdp_document_inode_checks (op, req, inode,
@@ -2977,7 +2977,7 @@ xdp_fuse_statfs (fuse_req_t req,
   int res;
   const char *op = "STATFS";
 
-  g_debug ("STATFS %lx", ino);
+  g_debug ("STATFS %" G_GINT64_MODIFIER "x", ino);
 
   if (!xdp_document_inode_checks (op, req, inode, 0))
     return;
@@ -3006,7 +3006,7 @@ xdp_fuse_setxattr (fuse_req_t req,
   g_autofree char *path = NULL;
   const char *op = "SETXATTR";
 
-  g_debug ("SETXATTR %lx %s", ino, name);
+  g_debug ("SETXATTR %" G_GINT64_MODIFIER "x %s", ino, name);
 
   if (!xdp_document_inode_checks (op, req, inode,
                                   CHECK_CAN_WRITE |
@@ -3041,7 +3041,7 @@ xdp_fuse_getxattr (fuse_req_t req,
   g_autofree char *path = NULL;
   const char *op = "GETXATTR";
 
-  g_debug ("GETXATTR %lx %s %ld", ino, name, size);
+  g_debug ("GETXATTR %" G_GINT64_MODIFIER "x %s %" G_GSIZE_FORMAT, ino, name, size);
 
   if (inode->domain->type != XDP_DOMAIN_DOCUMENT)
     return xdp_reply_err (op, req, ENODATA);
@@ -3082,7 +3082,7 @@ xdp_fuse_listxattr (fuse_req_t req,
   g_autofree char *path = NULL;
   const char *op = "LISTXATTR";
 
-  g_debug ("LISTXATTR %lx %ld", ino, size);
+  g_debug ("LISTXATTR %" G_GINT64_MODIFIER "x %" G_GSIZE_FORMAT, ino, size);
 
   if (inode->domain->type != XDP_DOMAIN_DOCUMENT)
     return xdp_reply_err (op, req, ENOTSUP);
@@ -3123,7 +3123,7 @@ xdp_fuse_removexattr (fuse_req_t req,
   ssize_t res;
   const char *op = "REMOVEXATTR";
 
-  g_debug ("REMOVEXATTR %lx %s", ino, name);
+  g_debug ("REMOVEXATTR %" G_GINT64_MODIFIER "x %s", ino, name);
 
   if (!xdp_document_inode_checks (op, req, inode,
                                   CHECK_CAN_WRITE |
@@ -3156,7 +3156,7 @@ xdp_fuse_getlk (fuse_req_t req,
   XdpFile *file = (XdpFile *)fi->fh;
   int res;
 
-  g_debug ("GETLK %lx", ino);
+  g_debug ("GETLK %" G_GINT64_MODIFIER "x", ino);
 
   res = fcntl (file->fd, F_GETLK, lock);
   if (res < 0)
@@ -3176,7 +3176,7 @@ xdp_fuse_setlk (fuse_req_t req,
   XdpFile *file = (XdpFile *)fi->fh;
   int res;
 
-  g_debug ("SETLK %lx", ino);
+  g_debug ("SETLK %" G_GINT64_MODIFIER "x", ino);
 
   res = fcntl (file->fd, F_SETLK, lock);
   if (res < 0)
@@ -3193,7 +3193,7 @@ xdp_fuse_flock (fuse_req_t req,
 {
   const char *op = "FLOCK";
 
-  g_debug ("FLOCK %lx", ino);
+  g_debug ("FLOCK %" G_GINT64_MODIFIER "x", ino);
 
   xdp_reply_err (op, req, ENOSYS);
 }