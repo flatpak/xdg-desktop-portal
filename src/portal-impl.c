flatpak/xdg-desktop-portal

commit d18c563b6a3442ea881e725eaa48e324bb85e8e0

portal-impl: Hard-code x-d-p-gtk as a last-resort fallback
x-d-p-gtk has historically been used as the portal implementation of
last resort, and in particular, users of assemble-it-yourself desktop
environments or otherwise-unsupported desktop environments have tended
to rely on it for basic functionality, particularly now that its
GNOME-specific parts have been removed.

We don't want to install a /usr/share/x-d-p/portals.conf that makes it
the fallback (as proposed in xdg-desktop-portal#1192) because that would
be higher-precedence than the fallback to the legacy UseIn mechanism.
For example, x-d-p-wlr has UseIn=...;Wayfire;... which means that it
will be used to provide screenshots and screencasts under Wayfire (even
without fixing WayfireWM/wayfire#1995), but installing a
/usr/share/x-d-p/portals.conf with only default=gtk would defeat that.

Instead, search for x-d-p-gtk as a lower precedence than the legacy
UseIn mechanism. This means that (for example) users of Wayfire will
get screenshots from x-d-p-wlr and file choosers from x-d-p-gtk unless
configured otherwise, while users of environments with no more appropriate
portal configuration at all (for example fvwm) will get x-d-p-gtk.

This change was previously a Debian-specific patch with a slightly
different warning.

Resolves: https://github.com/flatpak/xdg-desktop-portal/issues/1102
Signed-off-by: Simon McVittie <smcv@collabora.com>

src/portal-impl.c
@@ -587,19 +587,30 @@ find_portal_implementation (const char *interface)
         }
     }
 
-#if 0
-  /* Fall back to *any* installed implementation */
+  /* As a last resort, if nothing was selected for this desktop by
+   * ${desktop}-portals.conf or portals.conf, and no portal volunteered
+   * itself as suitable for this desktop via the legacy UseIn mechanism,
+   * try to fall back to x-d-p-gtk, which has historically been the portal
+   * UI backend used by desktop environments with no backend of their own.
+   * If it isn't installed, that is not an error: we just don't use it. */
   for (l = implementations; l != NULL; l = l->next)
     {
       PortalImplementation *impl = l->data;
 
+      if (!g_str_equal (impl->dbus_name, "org.freedesktop.impl.portal.desktop.gtk"))
+        continue;
+
       if (!g_strv_contains ((const char **)impl->interfaces, interface))
         continue;
 
-      g_debug ("Falling back to %s.portal for %s", impl->source, interface);
+      g_warning ("Choosing %s.portal for %s as a last-resort fallback",
+                 impl->source, interface);
+      g_warning_once ("The preferred method to match portal implementations "
+                      "to desktop environments is to use the portals.conf(5) "
+                      "configuration file");
+
       return impl;
     }
-#endif
 
   return NULL;
 }

commit 2e159e12c63b9ececcf798427b936144253ce9bc

portal-impl: Use fallback portals for Settings as well
Since 1.18.2, we have a hardcoded fallback to xdg-desktop-portal-gtk as
a last resort if no other portal is defined by a configuration file.
This fallback mechanism, however, isn't implemented for the
org.freedesktop.impl.portal.Settings interface.

The consequence is that everything seems to work just fine, but user
preferences like color scheme are ignored in applications that use the
portal API to retrieve them (not just flatpak apps, also libadwaita
apps). That comes as a surprise to users; and since the portal stuff
otherwise works fine, it's not entirely straightforward to figure out
that configuring portal.conf would fix it.

I believe that if find_portal_implementation implements fallbacks, so
should find_all_portal_implementations, for consistency and to avoid
surprises.

The fallback logic this commit implements closely resembles the logic in
find_portal_implementation: we don't look for fallbacks if there is a
portal configured in portals.conf (or if portals.conf explicitly says
there should be none), and we only use the x-d-p-gtk as a last resort if
we haven't found any impl(s) using the legacy UseIn key.

(The above should be a self-contained description but there's some
additional thoughts at
https://github.com/flatpak/xdg-desktop-portal/pull/1199#issuecomment-2093902804)

Fixes: d18c563b6a34 ("portal-impl: Hard-code x-d-p-gtk as a last-resort fallback")
Related: https://github.com/flatpak/xdg-desktop-portal/issues/1102

src/portal-impl.c
@@ -669,8 +669,10 @@ find_portal_implementation (const char *interface)
 GPtrArray *
 find_all_portal_implementations (const char *interface)
 {
+  const char **desktops;
   GPtrArray *impls;
   GList *l;
+  int i;
 
   impls = g_ptr_array_new ();
 
@@ -689,6 +691,57 @@ find_all_portal_implementations (const char *interface)
           g_debug ("Using %s.portal for %s (config)", impl->source, interface);
           g_ptr_array_add (impls, impl);
         }
+    }
+
+  if (impls->len)
+    return impls;
+
+  desktops = get_current_lowercase_desktops ();
+
+  /* Fallback to the old UseIn key */
+  for (i = 0; desktops[i] != NULL; i++)
+    {
+     for (l = implementations; l != NULL; l = l->next)
+        {
+          PortalImplementation *impl = l->data;
+
+          if (!g_strv_contains ((const char **)impl->interfaces, interface))
+            continue;
+
+          if (impl->use_in != NULL && g_strv_case_contains ((const char **)impl->use_in, desktops[i]))
+            {
+              g_warning ("Choosing %s.portal for %s via the deprecated UseIn key",
+                         impl->source, interface);
+              warn_please_use_portals_conf ();
+              g_debug ("Using %s.portal for %s in %s (fallback)", impl->source, interface, desktops[i]);
+              g_ptr_array_add (impls, impl);
+            }
+        }
+    }
+
+  if (impls->len)
+    return impls;
+
+  /* As a last resort, if nothing was selected for this desktop by
+   * ${desktop}-portals.conf or portals.conf, and no portal volunteered
+   * itself as suitable for this desktop via the legacy UseIn mechanism,
+   * try to fall back to x-d-p-gtk, which has historically been the portal
+   * UI backend used by desktop environments with no backend of their own.
+   * If it isn't installed, that is not an error: we just don't use it. */
+  for (l = implementations; l != NULL; l = l->next)
+    {
+      PortalImplementation *impl = l->data;
+
+      if (!g_str_equal (impl->dbus_name, "org.freedesktop.impl.portal.desktop.gtk"))
+        continue;
+
+      if (!g_strv_contains ((const char **)impl->interfaces, interface))
+        continue;
+
+      g_warning ("Choosing %s.portal for %s as a last-resort fallback",
+                 impl->source, interface);
+      warn_please_use_portals_conf ();
+      g_ptr_array_add (impls, impl);
     }
 
   return impls;