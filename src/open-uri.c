flatpak/xdg-desktop-portal

commit 39a34bae5b159593671093938b6e6786182fc4c5

open-uri: Pass activation token around
Accept an activation token from the frontend API, pass it to the
backend API to choose an application, and start the selected
application with it.

src/open-uri.c
@@ -227,6 +227,7 @@ launch_application_with_uri (const char *choice_id,
                              const char *uri,
                              const char *parent_window,
                              gboolean    writable,
+                             const char *activation_token,
                              GError    **error)
 {
   g_autofree char *desktop_id = g_strconcat (choice_id, ".desktop", NULL);
@@ -262,6 +263,9 @@ launch_application_with_uri (const char *choice_id,
     ruri = g_strdup (uri);
 
   g_app_launch_context_setenv (context, "PARENT_WINDOW_ID", parent_window);
+
+  if (activation_token)
+    g_app_launch_context_setenv (context, "XDG_ACTIVATION_TOKEN", activation_token);
 
   uris.data = (gpointer)ruri;
   uris.next = NULL;
@@ -355,6 +359,7 @@ send_response_in_thread_func (GTask *task,
       const char *parent_window;
       gboolean writable;
       const char *content_type;
+      const char *activation_token = NULL;
 
       g_debug ("Received choice %s", choice);
 
@@ -363,7 +368,9 @@ send_response_in_thread_func (GTask *task,
       writable = GPOINTER_TO_INT (g_object_get_data (G_OBJECT (request), "writable"));
       content_type = (const char *)g_object_get_data (G_OBJECT (request), "content-type");
 
-      if (launch_application_with_uri (choice, uri, parent_window, writable, NULL))
+      g_variant_lookup (options, "activation_token", "&s", &activation_token);
+
+      if (launch_application_with_uri (choice, uri, parent_window, writable, activation_token, NULL))
         update_permissions_store (xdp_app_info_get_id (request->app_info), content_type, choice);
     }
 
@@ -572,6 +579,7 @@ handle_open_in_thread_func (GTask *task,
   Request *request = (Request *)task_data;
   const char *parent_window;
   const char *app_id = xdp_app_info_get_id (request->app_info);
+  const char *activation_token;
   g_autofree char *uri = NULL;
   g_autoptr(XdpImplRequest) impl_request = NULL;
   g_autofree char *default_app = NULL;
@@ -599,6 +607,7 @@ handle_open_in_thread_func (GTask *task,
   writable = GPOINTER_TO_INT (g_object_get_data (G_OBJECT (request), "writable"));
   ask = GPOINTER_TO_INT (g_object_get_data (G_OBJECT (request), "ask"));
   open_dir = GPOINTER_TO_INT (g_object_get_data (G_OBJECT (request), "open-dir"));
+  activation_token = (const char *)g_object_get_data (G_OBJECT (request), "activation-token");
 
   REQUEST_AUTOLOCK (request);
 
@@ -764,7 +773,7 @@ handle_open_in_thread_func (GTask *task,
 
           g_debug ("Skipping app chooser");
 
-          gboolean result = launch_application_with_uri (app, uri, parent_window, writable, &error);
+          gboolean result = launch_application_with_uri (app, uri, parent_window, writable, activation_token, &error);
           if (request->exported)
             {
               if (!result)
@@ -794,6 +803,8 @@ handle_open_in_thread_func (GTask *task,
     g_variant_builder_add (&opts_builder, "{sv}", "filename", g_variant_new_string (basename));
   if (uri)
     g_variant_builder_add (&opts_builder, "{sv}", "uri", g_variant_new_string (uri));
+  if (activation_token)
+    g_variant_builder_add (&opts_builder, "{sv}", "activation_token", g_variant_new_string (uri));
 
   impl_request = xdp_impl_request_proxy_new_sync (g_dbus_proxy_get_connection (G_DBUS_PROXY (impl)),
                                                   G_DBUS_PROXY_FLAGS_NONE,
@@ -829,6 +840,7 @@ handle_open_uri (XdpOpenURI *object,
   g_autoptr(GTask) task = NULL;
   gboolean writable;
   gboolean ask;
+  const char *activation_token = NULL;
 
   if (xdp_impl_lockdown_get_disable_application_handlers (lockdown))
     {
@@ -846,11 +858,16 @@ handle_open_uri (XdpOpenURI *object,
   if (!g_variant_lookup (arg_options, "ask", "b", &ask))
     ask = FALSE;
 
+  g_variant_lookup (arg_options, "activation_token", "&s", &activation_token);
+
   g_object_set_data (G_OBJECT (request), "fd", GINT_TO_POINTER (-1));
   g_object_set_data_full (G_OBJECT (request), "uri", g_strdup (arg_uri), g_free);
   g_object_set_data_full (G_OBJECT (request), "parent-window", g_strdup (arg_parent_window), g_free);
   g_object_set_data (G_OBJECT (request), "writable", GINT_TO_POINTER (writable));
   g_object_set_data (G_OBJECT (request), "ask", GINT_TO_POINTER (ask));
+
+  if (activation_token)
+    g_object_set_data_full (G_OBJECT (request), "activation-token", g_strdup (activation_token), g_free);
 
   request_export (request, g_dbus_method_invocation_get_connection (invocation));
   xdp_open_uri_complete_open_uri (object, invocation, request->id);
@@ -875,6 +892,7 @@ handle_open_file (XdpOpenURI *object,
   gboolean writable;
   gboolean ask;
   int fd_id, fd;
+  const char *activation_token = NULL;
   g_autoptr(GError) error = NULL;
 
   if (xdp_impl_lockdown_get_disable_application_handlers (lockdown))
@@ -901,10 +919,15 @@ handle_open_file (XdpOpenURI *object,
       return TRUE;
     }
 
+  g_variant_lookup (arg_options, "activation_token", "&s", &activation_token);
+
   g_object_set_data (G_OBJECT (request), "fd", GINT_TO_POINTER (fd));
   g_object_set_data_full (G_OBJECT (request), "parent-window", g_strdup (arg_parent_window), g_free);
   g_object_set_data (G_OBJECT (request), "writable", GINT_TO_POINTER (writable));
   g_object_set_data (G_OBJECT (request), "ask", GINT_TO_POINTER (ask));
+
+  if (activation_token)
+    g_object_set_data_full (G_OBJECT (request), "activation-token", g_strdup (activation_token), g_free);
 
   request_export (request, g_dbus_method_invocation_get_connection (invocation));
   xdp_open_uri_complete_open_file (object, invocation, NULL, request->id);
@@ -927,6 +950,7 @@ handle_open_directory (XdpOpenURI *object,
   Request *request = request_from_invocation (invocation);
   g_autoptr(GTask) task = NULL;
   int fd_id, fd;
+  const char *activation_token = NULL;
   g_autoptr(GError) error = NULL;
 
   if (xdp_impl_lockdown_get_disable_application_handlers (lockdown))
@@ -947,11 +971,16 @@ handle_open_directory (XdpOpenURI *object,
       return TRUE;
     }
 
+  g_variant_lookup (arg_options, "activation_token", "&s", &activation_token);
+
   g_object_set_data (G_OBJECT (request), "fd", GINT_TO_POINTER (fd));
   g_object_set_data_full (G_OBJECT (request), "parent-window", g_strdup (arg_parent_window), g_free);
   g_object_set_data (G_OBJECT (request), "writable", GINT_TO_POINTER (0));
   g_object_set_data (G_OBJECT (request), "ask", GINT_TO_POINTER (0));
   g_object_set_data (G_OBJECT (request), "open-dir", GINT_TO_POINTER (1));
+
+  if (activation_token)
+    g_object_set_data_full (G_OBJECT (request), "activation-token", g_strdup (activation_token), g_free);
 
   request_export (request, g_dbus_method_invocation_get_connection (invocation));
   xdp_open_uri_complete_open_file (object, invocation, NULL, request->id);

commit b6d123fa05983b113ab8bc9039b4bebd84fc5515

open-uri: bump advertised interface version
OpenURI in version 4 added support for activation token, but it still
advertises older version on DBus. This might make clients checking the
version not to use the activation token.

Fixes: 39a34ba ("open-uri: Pass activation token around")

src/open-uri.c
@@ -1097,7 +1097,7 @@ open_uri_iface_init (XdpDbusOpenURIIface *iface)
 static void
 open_uri_init (OpenURI *openuri)
 {
-  xdp_dbus_open_uri_set_version (XDP_DBUS_OPEN_URI (openuri), 3);
+  xdp_dbus_open_uri_set_version (XDP_DBUS_OPEN_URI (openuri), 4);
 }
 
 static void

tests/test-portals.c
@@ -448,7 +448,7 @@ DEFINE_TEST_EXISTS(inhibit, INHIBIT, 3)
 DEFINE_TEST_EXISTS(location, LOCATION, 1)
 DEFINE_TEST_EXISTS(network_monitor, NETWORK_MONITOR, 3)
 DEFINE_TEST_EXISTS(notification, NOTIFICATION, 1)
-DEFINE_TEST_EXISTS(open_uri, OPEN_URI, 3)
+DEFINE_TEST_EXISTS(open_uri, OPEN_URI, 4)
 DEFINE_TEST_EXISTS(print, PRINT, 3)
 DEFINE_TEST_EXISTS(proxy_resolver, PROXY_RESOLVER, 1)
 DEFINE_TEST_EXISTS(screenshot, SCREENSHOT, 2)