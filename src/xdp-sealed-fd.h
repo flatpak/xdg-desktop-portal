83bc80f9ca4d5addc319df1531a51dbe805fa3bb
Julian Sparber <julian@sparber.net>
2024-07-12 15:51:47 +0200

    xdp-sealed-fd: Introduce an object holding a sealed fd
    This object is used to to store GBytes that need to be passed between
    processes. In future it will also be used to share data between clients,
    the frontend and backend.
    
    The introduced object handles creating and sealing of memfds. It uses
    memfd_create() to create a new fd in case GBytes are provided and seals
    it.
    It also takes already existing memfds and ensures all seals that prevent
    data modifications are applied.

---
+++ src/xdp-sealed-fd.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2024 GNOME Foundation Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <glib-object.h>
+
+#define XDP_TYPE_SEALED_FD (xdp_sealed_fd_get_type())
+G_DECLARE_FINAL_TYPE (XdpSealedFd,
+                      xdp_sealed_fd,
+                      XDP, SEALED_FD,
+                      GObject)
+
+XdpSealedFd * xdp_sealed_fd_new_take_memfd (int           memfd,
+                                            GError      **error);
+XdpSealedFd * xdp_sealed_fd_new_from_bytes (GBytes       *bytes,
+                                            GError      **error);
+XdpSealedFd * xdp_sealed_fd_new_from_handle (GVariant     *handle,
+                                             GUnixFDList  *fd_list,
+                                             GError      **error);
+int xdp_sealed_fd_get_fd (XdpSealedFd  *sealed_fd);
+int xdp_sealed_fd_dup_fd (XdpSealedFd  *sealed_fd);
+
