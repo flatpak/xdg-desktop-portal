1b5a16932493324fa0f21cfb1c3f9c1d16cb6a19
Philip Withnall <pwithnall@gnome.org>
2024-09-27 14:35:30 +0100

    src: Fix license blurb and add SPDX-License-Identifier to some files
    See the previous commit for reasoning.
    
    This extends the same methodology to the source files in
    `document-portal`, `src` and `tests`: fix the version number for files
    which look like they were meant to be LGPL-2.1-licensed, and add an
    `SPDX-License-Identifier` to them at the same time.
    
    I haven’t touched any files which didn’t have the incorrect LGPL
    version.
    
    Signed-off-by: Philip Withnall <pwithnall@gnome.org>

--- document-portal/permission-db.c
+++ document-portal/permission-db.c
@@ -2,9 +2,11 @@
  *
  * Copyright (C) 2015 Red Hat, Inc
  *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
  * This file is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2 of the
+ * published by the Free Software Foundation; either version 2.1 of the
  * License, or (at your option) any later version.
  *
  * This file is distributed in the hope that it will be useful, but
