fbd54aa3d1ce1efd2f8282a6327e693bbedff35c
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2024-10-04 10:58:18 -0300

    app-info: Turn "pidfd" a construct-only property
    And pass it during object construction (i.e. g_initable_new())

--- src/xdp-app-info-flatpak.c
+++ src/xdp-app-info-flatpak.c
@@ -826,10 +826,10 @@ xdp_app_info_flatpak_new (int      pid,
                                      "engine", FLATPAK_ENGINE_ID,
                                      "id", id,
                                      "instance", instance,
+                                     "pidfd", bwrap_pidfd,
                                      NULL);
 
-  xdp_app_info_initialize (XDP_APP_INFO (app_info_flatpak),
-                           bwrap_pidfd, gappinfo, flags);
+  xdp_app_info_initialize (XDP_APP_INFO (app_info_flatpak), gappinfo, flags);
 
   app_info_flatpak->flatpak_info = g_steal_pointer (&metadata);
 
