00a8c1d05ddc4d17e3a2610af728db6d5fedd2e3
Sebastian Wick <sebastian.wick@redhat.com>
2024-10-17 13:34:56 +0200

    app-info/test: Allow passing USB queries via env
    We want to test the USB portal which requires USB queries to determine
    which USB devices should be enumerable and could potentially be
    acquired. This adds an environment variable similar to the one for the
    app id that the test harness can set.

--- src/xdp-app-info-test.c
+++ src/xdp-app-info-test.c
@@ -21,9 +21,13 @@
 
 #include "xdp-app-info-test-private.h"
 
+#include "xdp-usb-query.h"
+
 struct _XdpAppInfoTest
 {
   XdpAppInfo parent;
+
+  GPtrArray *usb_queries;
 };
 
 G_DEFINE_FINAL_TYPE (XdpAppInfoTest, xdp_app_info_test, XDP_TYPE_APP_INFO)
@@ -46,15 +50,38 @@ xdp_app_info_test_validate_dynamic_launcher (XdpAppInfo  *app_info,
   return TRUE;
 }
 
+static const GPtrArray *
+xdp_app_info_test_get_usb_queries (XdpAppInfo *app_info)
+{
+  XdpAppInfoTest *app_info_test = XDP_APP_INFO_TEST (app_info);
+
+  return app_info_test->usb_queries;
+}
+
+static void
+xdp_app_info_test_dispose (GObject *object)
+{
+  XdpAppInfoTest *app_info = XDP_APP_INFO_TEST (object);
+
+  g_clear_pointer (&app_info->usb_queries, g_ptr_array_unref);
+
+  G_OBJECT_CLASS (xdp_app_info_test_parent_class)->dispose (object);
+}
+
 static void
 xdp_app_info_test_class_init (XdpAppInfoTestClass *klass)
 {
   XdpAppInfoClass *app_info_class = XDP_APP_INFO_CLASS (klass);
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->dispose = xdp_app_info_test_dispose;
 
   app_info_class->validate_autostart =
     xdp_app_info_test_validate_autostart;
   app_info_class->validate_dynamic_launcher =
     xdp_app_info_test_validate_dynamic_launcher;
+  app_info_class->get_usb_queries =
+    xdp_app_info_test_get_usb_queries;
 }
 
 static void
@@ -62,8 +89,38 @@ xdp_app_info_test_init (XdpAppInfoTest *app_info_test)
 {
 }
 
+static GPtrArray *
+parse_usb_queries_string (const char *usb_queries_str)
+{
+  g_autoptr(GPtrArray) usb_queries = NULL;
+  g_auto(GStrv) queries_strs = NULL;
+
+  if (!usb_queries_str)
+    return NULL;
+
+  usb_queries =
+    g_ptr_array_new_with_free_func ((GDestroyNotify) xdp_usb_query_free);
+
+  queries_strs = g_strsplit (usb_queries_str, ";", 0);
+  for (size_t i = 0; queries_strs[i] != NULL; i++)
+    {
+      g_autoptr(XdpUsbQuery) query =
+        xdp_usb_query_from_string (XDP_USB_QUERY_TYPE_ENUMERABLE,
+                                   queries_strs[i]);
+
+      if (query)
+        g_ptr_array_add (usb_queries, g_steal_pointer (&query));
+    }
+
+  if (usb_queries->len == 0)
+    return NULL;
+
+  return g_steal_pointer (&usb_queries);
+}
+
 XdpAppInfo *
-xdp_app_info_test_new (const char *app_id)
+xdp_app_info_test_new (const char *app_id,
+                       const char *usb_queries_str)
 {
   g_autoptr (XdpAppInfoTest) app_info_test = NULL;
 
@@ -72,6 +129,8 @@ xdp_app_info_test_new (const char *app_id)
                            "", app_id, NULL,
                            -1, NULL,
                            TRUE, TRUE, TRUE);
+
+  app_info_test->usb_queries = parse_usb_queries_string (usb_queries_str);
 
   return XDP_APP_INFO (g_steal_pointer (&app_info_test));
 }
