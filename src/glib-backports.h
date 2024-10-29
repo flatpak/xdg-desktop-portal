b8d4403aa4e579b844b8635a567c52e9a6fd44b3
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2024-10-29 13:47:24 -0300

    glib-backports: Pull in g_autofd and g_clear_fd

--- src/glib-backports.h
+++ src/glib-backports.h
@@ -24,6 +24,49 @@
 #pragma once
 
 #include <gio/gio.h>
+#include <glib/gstdio.h>
+
+#if !GLIB_CHECK_VERSION(2, 76, 0)
+
+static inline gboolean
+g_clear_fd (int     *fd_ptr,
+            GError **error)
+{
+  int fd = *fd_ptr;
+
+  *fd_ptr = -1;
+
+  if (fd < 0)
+    return TRUE;
+
+  /* Suppress "Not available before" warning */
+  G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+  return g_close (fd, error);
+  G_GNUC_END_IGNORE_DEPRECATIONS
+}
+
+static inline void
+_g_clear_fd_ignore_error (int *fd_ptr)
+{
+  /* Don't overwrite thread-local errno if closing the fd fails */
+  int errsv = errno;
+
+  /* Suppress "Not available before" warning */
+  G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+
+  if (!g_clear_fd (fd_ptr, NULL))
+    {
+      /* Do nothing: we ignore all errors, except for EBADF which
+       * is a programming error, checked for by g_close(). */
+    }
+
+  G_GNUC_END_IGNORE_DEPRECATIONS
+
+  errno = errsv;
+}
+
+#define g_autofd __attribute__((cleanup(_g_clear_fd_ignore_error)))
+#endif
 
 #if !GLIB_CHECK_VERSION (2, 68, 0)
 guint g_string_replace (GString     *string,
