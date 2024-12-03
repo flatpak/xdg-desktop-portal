a6f80da4add6b4b130d4f88c25093d16223750e3
Sebastian Wick <sebastian.wick@redhat.com>
2024-12-03 20:18:06 +0100

    tests/py: Use the new test helpers
    Now that we have the test fixtures and the new test API in place, we can
    move our existing tests over to it.

--- tests/test_clipboard.py
+++ tests/test_clipboard.py
@@ -2,7 +2,8 @@
 #
 # This file is formatted with Python Black
 
-from tests import Session
+import tests as xdp
+
 import dbus
 import pytest
 import os
@@ -22,59 +23,63 @@ def required_templates():
 
 
 class TestClipboard:
-    def test_version(self, portal_mock):
-        portal_mock.check_version(1)
+    def test_version(self, portals, dbus_con):
+        xdp.check_version(dbus_con, "Clipboard", 1)
 
-    def start_session(self, pmock):
-        create_session_request = pmock.create_request("RemoteDesktop")
+    def start_session(self, dbus_con):
+        clipboard_intf = xdp.get_portal_iface(dbus_con, "Clipboard")
+        remotedesktop_intf = xdp.get_portal_iface(dbus_con, "RemoteDesktop")
+
+        create_session_request = xdp.Request(dbus_con, remotedesktop_intf)
         create_session_response = create_session_request.call(
             "CreateSession", options={"session_handle_token": "1234"}
         )
+        assert create_session_response
         assert create_session_response.response == 0
         assert str(create_session_response.results["session_handle"])
 
-        session = Session.from_response(pmock.dbus_con, create_session_response)
+        session = xdp.Session.from_response(dbus_con, create_session_response)
 
-        clipboard_interface = pmock.get_dbus_interface()
-        clipboard_interface.RequestClipboard(session.handle, {})
+        clipboard_intf.RequestClipboard(session.handle, {})
 
-        start_session_request = pmock.create_request("RemoteDesktop")
+        start_session_request = xdp.Request(dbus_con, remotedesktop_intf)
         start_session_response = start_session_request.call(
             "Start", session_handle=session.handle, parent_window="", options={}
         )
 
+        assert start_session_response
         assert start_session_response.response == 0
 
         return (session, start_session_response.results.get("clipboard_enabled"))
 
-    def test_request_clipboard_and_start_session(self, portal_mock):
-        _, clipboard_enabled = self.start_session(portal_mock)
+    def test_request_clipboard_and_start_session(self, portals, dbus_con):
+        _, clipboard_enabled = self.start_session(dbus_con)
 
         assert clipboard_enabled
 
     @pytest.mark.parametrize(
         "template_params", ({"RemoteDesktop": {"force-clipboard-enabled": False}},)
     )
-    def test_clipboard_checks_clipboard_enabled(self, portal_mock):
-        session, clipboard_enabled = self.start_session(portal_mock)
-        clipboard_interface = portal_mock.get_dbus_interface()
+    def test_clipboard_checks_clipboard_enabled(self, portals, dbus_con):
+        clipboard_intf = xdp.get_portal_iface(dbus_con, "Clipboard")
+        session, clipboard_enabled = self.start_session(dbus_con)
 
         assert not clipboard_enabled
 
         with pytest.raises(dbus.exceptions.DBusException):
-            clipboard_interface.SetSelection(session.handle, {})
+            clipboard_intf.SetSelection(session.handle, {})
 
-    def test_clipboard_set_selection(self, portal_mock):
-        session, _ = self.start_session(portal_mock)
-        clipboard_interface = portal_mock.get_dbus_interface()
+    def test_clipboard_set_selection(self, portals, dbus_con):
+        clipboard_intf = xdp.get_portal_iface(dbus_con, "Clipboard")
+        session, _ = self.start_session(dbus_con)
 
-        clipboard_interface.SetSelection(session.handle, {})
+        clipboard_intf.SetSelection(session.handle, {})
 
-    def test_clipboard_selection_write(self, portal_mock):
-        session, _ = self.start_session(portal_mock)
-        clipboard_interface = portal_mock.get_dbus_interface()
+    def test_clipboard_selection_write(self, portals, dbus_con):
+        clipboard_intf = xdp.get_portal_iface(dbus_con, "Clipboard")
+        session, _ = self.start_session(dbus_con)
 
-        fd_object: dbus.types.UnixFd = clipboard_interface.SelectionWrite(
+        fd_object: dbus.types.UnixFd = clipboard_intf.SelectionWrite(
             session.handle, 1234
         )
         assert fd_object
@@ -85,13 +90,13 @@ class TestClipboard:
         bytes_written = os.write(fd, b"Clipboard")
         assert bytes_written > 0
 
-        clipboard_interface.SelectionWriteDone(session.handle, 1234, True)
+        clipboard_intf.SelectionWriteDone(session.handle, 1234, True)
 
-    def test_clipboard_selection_read(self, portal_mock):
-        session, _ = self.start_session(portal_mock)
-        clipboard_interface = portal_mock.get_dbus_interface()
+    def test_clipboard_selection_read(self, portals, dbus_con):
+        clipboard_intf = xdp.get_portal_iface(dbus_con, "Clipboard")
+        session, _ = self.start_session(dbus_con)
 
-        fd_object: dbus.types.UnixFd = clipboard_interface.SelectionRead(
+        fd_object: dbus.types.UnixFd = clipboard_intf.SelectionRead(
             session.handle, "mimetype"
         )
         assert fd_object
@@ -99,5 +104,5 @@ class TestClipboard:
         fd = fd_object.take()
         assert fd
 
-        clipboard = os.read(fd, 1000)
-        assert str(clipboard)
+        clipboard_contents = os.read(fd, 1000)
+        assert str(clipboard_contents)
