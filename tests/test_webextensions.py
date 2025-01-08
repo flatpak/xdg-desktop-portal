915ead76e2beb9b8b6bb9ac51a1fffbc667d4282
Jan Horak <jhorak@redhat.com>
2025-01-08 10:15:08 +0100

    Fixed python lints

--- tests/test_webextensions.py
+++ tests/test_webextensions.py
@@ -1,11 +1,8 @@
-import os
-import time
 import tests as xdp
 import random
 import dbus
-from gi.repository import GLib, Gio
-import pytest
 import json
+
 
 def set_web_extensions_permissions(permission, name, app_id, dbus_con):
     permission_store = xdp.get_permission_store_iface(dbus_con)
@@ -16,6 +13,7 @@ def set_web_extensions_permissions(permission, name, app_id, dbus_con):
     except dbus.DBusException as e:
         raise RuntimeError(f"Failed to set permissions: {e}")
 
+
 class TestWebExtensions:
     def test_version(self, portals, dbus_con):
         xdp.check_version(dbus_con, "WebExtensions", 1)
@@ -23,10 +21,7 @@ class TestWebExtensions:
     def create_session(self, dbus_con):
         webextensions_intf = xdp.get_portal_iface(dbus_con, "WebExtensions")
         session_token = "portal" + str(random.randint(0, 100000))
-        options = {
-            "mode": "mozilla",
-            "session_handle_token": session_token
-        }
+        options = {"mode": "mozilla", "session_handle_token": session_token}
         session = xdp.Session(
             dbus_con,
             webextensions_intf.CreateSession(options),
@@ -36,30 +31,25 @@ class TestWebExtensions:
     def get_manifest(self, name, extension_or_origin, dbus_con):
         webextensions_intf = xdp.get_portal_iface(dbus_con, "WebExtensions")
         return webextensions_intf.GetManifest(
-            self.session_handle,
-            name,
-            extension_or_origin
+            self.session_handle, name, extension_or_origin
         )
 
     def start(self, name, extension_or_origin, dbus_con):
         webextensions_intf = xdp.get_portal_iface(dbus_con, "WebExtensions")
         request = xdp.Request(dbus_con, webextensions_intf)
         return request.call(
-                "Start", 
-                session_handle=self.session_handle, 
-                name=name, 
-                extension_or_origin=extension_or_origin,
-                options={}
+            "Start",
+            session_handle=self.session_handle,
+            name=name,
+            extension_or_origin=extension_or_origin,
+            options={},
         )
 
     def get_pipes(self, dbus_con):
         webextensions_intf = xdp.get_portal_iface(dbus_con, "WebExtensions")
-        response = webextensions_intf.GetPipes(
-            self.session_handle,
-            {}
-        )
+        response = webextensions_intf.GetPipes(self.session_handle, {})
         return response
-    
+
     def test_web_extensions_basic(self, portals, app_id, dbus_con):
         name = "org.example.testing"
         set_web_extensions_permissions("yes", name, app_id, dbus_con)
@@ -72,9 +62,10 @@ class TestWebExtensions:
         manifest_data = self.get_manifest(name, extension_or_origin, dbus_con)
         assert manifest_data, "Manifest should not be None"
 
-        manifest =json.loads(manifest_data)
-        assert "some-extension@example.org" in manifest['allowed_extensions'], \
-                "Extension ID missing in manifest"
+        manifest = json.loads(manifest_data)
+        assert (
+            "some-extension@example.org" in manifest["allowed_extensions"]
+        ), "Extension ID missing in manifest"
 
         response = self.start(name, extension_or_origin, dbus_con)
         assert response, "Response cannot be None"
@@ -98,9 +89,10 @@ class TestWebExtensions:
         manifest_data = self.get_manifest(name, extension_or_origin, dbus_con)
         assert manifest_data, "Manifest should not be None"
 
-        manifest =json.loads(manifest_data)
-        assert "some-extension@example.org" in manifest['allowed_extensions'], \
-                "Extension ID missing in manifest"
+        manifest = json.loads(manifest_data)
+        assert (
+            "some-extension@example.org" in manifest["allowed_extensions"]
+        ), "Extension ID missing in manifest"
 
         response = self.start(name, extension_or_origin, dbus_con)
         print(response)
@@ -111,22 +103,24 @@ class TestWebExtensions:
         try:
             pipes = self.get_pipes(dbus_con)
         except dbus.DBusException as e:
-            assert e.get_dbus_name() == "org.freedesktop.DBus.Error.AccessDenied", "unknown exception"
-        assert pipes == None, "Expected pipes == None"
+            assert (
+                e.get_dbus_name() == "org.freedesktop.DBus.Error.AccessDenied"
+            ), "unknown exception"
+        assert pipes is None, "Expected pipes is None"
 
     def test_web_extensions_bad_name(self, portals, app_id, dbus_con):
         bad_extension_names = ["no-dashes", "../foo", "no_trailing_dot."]
 
         name = "org.example.testing"
         set_web_extensions_permissions("yes", name, app_id, dbus_con)
-        
+
         self.session_handle = self.create_session(dbus_con)
         assert self.session_handle, "Session handle should not be None"
 
         extension_or_origin = "some-extension@example.org"
 
         for extension_or_origin in bad_extension_names:
-          self.session_handle = self.create_session(dbus_con)
-          response = self.start(name, extension_or_origin, dbus_con)
-          assert response, "Response cannot be None"
-          assert response.response == 2, "Returned invalid response, expected 2"
+            self.session_handle = self.create_session(dbus_con)
+            response = self.start(name, extension_or_origin, dbus_con)
+            assert response, "Response cannot be None"
+            assert response.response == 2, "Returned invalid response, expected 2"
