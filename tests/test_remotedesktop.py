4522890cfaa7e68cedddb3ad578be1201d905a8b
Peter Hutterer <peter.hutterer@who-t.net>
2025-01-10 15:27:06 +1000

    session: Validate the token for correctness
    The token is used as part of an object path so it has to meet those
    requirements. We can't escape it since the caller presumably expects to
    use the token as-is so where it fails the validity simply error out.
    
    Note that we do not allow the token to create a subpath, so a slash
    in the token value is disallowed even though technically this could
    work.
    
    Closes: https://github.com/flatpak/xdg-desktop-portal/issues/1549

--- tests/test_remotedesktop.py
+++ tests/test_remotedesktop.py
@@ -46,6 +46,20 @@ class TestRemoteDesktop:
         session.close()
         xdp.wait_for(lambda: session.closed)
 
+    @pytest.mark.parametrize("token", ("Invalid-Token&", "", "/foo"))
+    def test_remote_desktop_create_session_invalid(self, portals, dbus_con, token):
+        remotedesktop_intf = xdp.get_portal_iface(dbus_con, "RemoteDesktop")
+
+        request = xdp.Request(dbus_con, remotedesktop_intf)
+        options = {"session_handle_token": token}
+
+        with pytest.raises(dbus.exceptions.DBusException) as excinfo:
+            request.call("CreateSession", options=options)
+
+        e = excinfo.value
+        assert e.get_dbus_name() == "org.freedesktop.portal.Error.InvalidArgument"
+        assert "Invalid token" in e.get_dbus_message()
+
     @pytest.mark.parametrize(
         "template_params", ({"remotedesktop": {"force-close": 500}},)
     )
