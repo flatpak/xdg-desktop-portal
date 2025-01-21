d8980ef3a9c2bd6d0d3fa6a295aa64a35b020c5f
Sebastian Wick <sebastian.wick@redhat.com>
2025-01-21 02:30:14 +0100

    tests/py: Move fuse check to xdp helper file

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,13 +7,14 @@
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib, Gio
 from itertools import count
-from typing import Any, Dict, Optional, NamedTuple, Callable, List
+from typing import Any, Dict, Optional, NamedTuple, Callable, List, Tuple
 
 import os
 import dbus
 import dbus.proxies
 import dbusmock
 import logging
+import subprocess
 
 
 DBusGMainLoop(set_as_default=True)
@@ -41,6 +42,32 @@ def is_in_container() -> bool:
 
 def run_long_tests() -> bool:
     return os.environ.get("XDP_TEST_RUN_LONG") is not None
+
+
+def check_program_success(cmd) -> bool:
+    proc = subprocess.Popen(
+        cmd, stdout=None, stderr=None, shell=True, universal_newlines=True
+    )
+    _ = proc.communicate()
+    return proc.returncode == 0
+
+
+def can_run_fuse() -> Tuple[bool, str]:
+    if not check_program_success("fusermount3 --version"):
+        return (False, "no fusermount3")
+
+    if not check_program_success(
+        "capsh --print | grep -q 'Bounding set.*[^a-z]cap_sys_admin'"
+    ):
+        return (False, "No cap_sys_admin in bounding set, can't use FUSE")
+
+    if not check_program_success("[ -w /dev/fuse ]"):
+        return (False, "no write access to /dev/fuse")
+
+    if not check_program_success("[ -e /etc/mtab ]"):
+        return (False, "no /etc/mtab")
+
+    return (True, "success")
 
 
 def wait(ms: int):
