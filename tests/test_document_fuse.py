fd44d75c9c45b8df6ddb881d107aa39c23885a2f
Sebastian Wick <sebastian.wick@redhat.com>
2025-02-04 13:30:55 +0100

    tests/document_fuse: Use pytest.raises instead of open coding it
    The code also had a bug where its using instanceof on a class instead of
    an instance, but then also accidentally check for the negated of what it
    was supposed to check. Found by mypy because it complained that the
    variable might not have been of the right type.

--- tests/test_document_fuse.py
+++ tests/test_document_fuse.py
@@ -79,67 +79,35 @@ DOCUMENT_ADD_FLAGS_DIRECTORY = 1 << 3
 
 
 def assertRaises(exc_type, func, *args, **kwargs):
-    raised_exc = None
-    try:
+    with pytest.raises(exc_type):
         func(*args, **kwargs)
-    except Exception:
-        raised_exc = sys.exc_info()[0]
-
-    if not raised_exc:
-        raise AssertionError("{0} was not raised".format(exc_type.__name__))
-    if raised_exc != exc_type:
-        raise AssertionError(
-            "Wrong assertion type {0} was raised instead of {1}".format(
-                raised_exc.__name__, exc_type.__name__
-            )
-        )
 
 
 def assertRaisesErrno(error_nr, func, *args, **kwargs):
-    raised_exc = None
-    raised_exc_value = None
-    try:
+    with pytest.raises(OSError) as excinfo:
         func(*args, **kwargs)
-    except Exception:
-        raised_exc = sys.exc_info()[0]
-        raised_exc_value = sys.exc_info()[1]
 
-    if not raised_exc:
-        raise AssertionError("No assertion was raised")
-    if isinstance(raised_exc, OSError):
-        raise AssertionError("OSError was not raised")
-    if raised_exc_value.errno != error_nr:
+    if excinfo.value.errno != error_nr:
         raise AssertionError(
             "Wrong errno {0} was raised instead of {1}".format(
-                raised_exc_value.errno, error_nr
+                excinfo.value.errno, error_nr
             )
         )
 
 
 def assertRaisesGError(message, code, func, *args, **kwargs):
-    raised_exc = None
-    raised_exc_value = None
-    try:
+    with pytest.raises(GLib.GError) as excinfo:
         func(*args, **kwargs)
-    except Exception:
-        raised_exc = sys.exc_info()[0]
-        raised_exc_value = sys.exc_info()[1]
 
-    if not raised_exc:
-        raise AssertionError("No assertion was raised")
-    if raised_exc != GLib.GError:
-        raise AssertionError("GError was not raised")
-    if not raised_exc_value.message.startswith(message):
+    if not excinfo.value.message.startswith(message):
         raise AssertionError(
             "Wrong message {0} doesn't start with {1}".format(
-                raised_exc_value.message, message
+                excinfo.value.message, message
             )
         )
-    if raised_exc_value.code != code:
+    if excinfo.value.code != code:
         raise AssertionError(
-            "Wrong code {0} was raised instead of {1}".format(
-                raised_exc_value.code, code
-            )
+            "Wrong code {0} was raised instead of {1}".format(excinfo.value.code, code)
         )
 
 
