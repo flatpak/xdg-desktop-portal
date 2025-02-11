c463f4395f4fd5b3ee7cfd63b477eb24addc2c04
Sebastian Wick <sebastian.wick@redhat.com>
2025-02-11 16:10:19 +0100

    app-info: Reorder struct and group fields
    This just creates a bit more order.

--- src/xdp-app-info.c
+++ src/xdp-app-info.c
@@ -60,16 +60,23 @@ G_DEFINE_QUARK (XdpAppInfo, xdp_app_info_error);
 
 typedef struct _XdpAppInfoPrivate
 {
+  /* identity */
   char *engine;
   char *id;
   char *instance;
-  int pidfd;
+
+  /* app info */
   GAppInfo *gappinfo;
-  XdpAppInfoFlags flags;
+
+  /* calling process */
+  int pidfd;
 
   /* pid namespace mapping */
   GMutex pidns_lock;
   ino_t pidns_id;
+
+  /* misc */
+  XdpAppInfoFlags flags;
 } XdpAppInfoPrivate;
 
 G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE (XdpAppInfo, xdp_app_info, G_TYPE_OBJECT)
