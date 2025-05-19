flatpak/xdg-desktop-portal

commit dd675776c684f8fe2e92d675d82f38250b12c32b

app-info: Add a stub GInitable implementation
We'll start shuffling things around, so that XdpAppInfo & subclasses
operate with the mechanics proposed by GInitable.

src/xdp-app-info-flatpak.c
@@ -820,7 +820,11 @@ xdp_app_info_flatpak_new (int      pid,
   if (has_network)
     flags |= XDP_APP_INFO_FLAG_HAS_NETWORK;
 
-  app_info_flatpak = g_object_new (XDP_TYPE_APP_INFO_FLATPAK, NULL);
+  app_info_flatpak = g_initable_new (XDP_TYPE_APP_INFO_FLATPAK,
+                                     NULL,
+                                     error,
+                                     NULL);
+
   xdp_app_info_initialize (XDP_APP_INFO (app_info_flatpak),
                            FLATPAK_ENGINE_ID, id, instance,
                            bwrap_pidfd, gappinfo, flags);

src/xdp-app-info-test.c
@@ -133,7 +133,11 @@ xdp_app_info_test_new (const char *app_id,
 {
   g_autoptr (XdpAppInfoTest) app_info_test = NULL;
 
-  app_info_test = g_object_new (XDP_TYPE_APP_INFO_TEST, NULL);
+  app_info_test = g_initable_new (XDP_TYPE_APP_INFO_TEST,
+                                  NULL,
+                                  NULL,
+                                  NULL);
+
   xdp_app_info_initialize (XDP_APP_INFO (app_info_test),
                            "", app_id, NULL,
                            -1, NULL,

src/xdp-app-info-host.c
@@ -186,13 +186,18 @@ get_appid_from_pid (pid_t pid)
 }
 
 static XdpAppInfoHost *
-xdp_app_info_host_new_full (const char *app_id,
-                            int         pidfd,
-                            GAppInfo   *gappinfo)
+xdp_app_info_host_new_full (const char  *app_id,
+                            int          pidfd,
+                            GAppInfo    *gappinfo,
+                            GError     **error)
 {
   XdpAppInfoHost *app_info_host;
 
-  app_info_host = g_object_new (XDP_TYPE_APP_INFO_HOST, NULL);
+  app_info_host = g_initable_new (XDP_TYPE_APP_INFO_HOST,
+                                  NULL,
+                                  error,
+                                  NULL);
+
   xdp_app_info_initialize (XDP_APP_INFO (app_info_host),
                            /* engine, app id, instance */
                            NULL, app_id, NULL,
@@ -220,7 +225,10 @@ xdp_app_info_host_new_registered (int          pidfd,
       return NULL;
     }
 
-  return XDP_APP_INFO (xdp_app_info_host_new_full (app_id, pidfd, gappinfo));
+  return XDP_APP_INFO (xdp_app_info_host_new_full (app_id,
+                                                   pidfd,
+                                                   gappinfo,
+                                                   error));
 }
 
 XdpAppInfo *
@@ -235,5 +243,8 @@ xdp_app_info_host_new (int pid,
   desktop_id = g_strconcat (app_id, ".desktop", NULL);
   gappinfo = G_APP_INFO (g_desktop_app_info_new (desktop_id));
 
-  return XDP_APP_INFO (xdp_app_info_host_new_full (app_id, pidfd, gappinfo));
+  return XDP_APP_INFO (xdp_app_info_host_new_full (app_id,
+                                                   pidfd,
+                                                   gappinfo,
+                                                   NULL));
 }

src/xdp-app-info-snap.c
@@ -226,7 +226,11 @@ xdp_app_info_snap_new (int      pid,
   if (has_network)
     flags |= XDP_APP_INFO_FLAG_HAS_NETWORK;
 
-  app_info_snap = g_object_new (XDP_TYPE_APP_INFO_SNAP, NULL);
+  app_info_snap = g_initable_new (XDP_TYPE_APP_INFO_SNAP,
+                                  NULL,
+                                  error,
+                                  NULL);
+
   xdp_app_info_initialize (XDP_APP_INFO (app_info_snap),
                            "io.snapcraft", snap_id, NULL,
                            pidfd, gappinfo, flags);

src/xdp-app-info.c
@@ -72,7 +72,25 @@ typedef struct _XdpAppInfoPrivate
   ino_t pidns_id;
 } XdpAppInfoPrivate;
 
-G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE (XdpAppInfo, xdp_app_info, G_TYPE_OBJECT)
+static void g_initable_init_iface (GInitableIface *iface);
+
+G_DEFINE_ABSTRACT_TYPE_WITH_CODE (XdpAppInfo, xdp_app_info, G_TYPE_OBJECT,
+                                  G_ADD_PRIVATE (XdpAppInfo)
+                                  G_IMPLEMENT_INTERFACE (G_TYPE_INITABLE, g_initable_init_iface))
+
+static gboolean
+xdp_app_info_initable_init (GInitable     *initable,
+                            GCancellable  *cancellable,
+                            GError       **error)
+{
+  return TRUE;
+}
+
+static void
+g_initable_init_iface (GInitableIface *iface)
+{
+  iface->init = xdp_app_info_initable_init;
+}
 
 static void
 xdp_app_info_dispose (GObject *object)

commit 9118db6b37958fa00c7545299b4e103f181e46ca

app-info: Check that g_initable_new succeeds before modifying
Fixes: dd675776 ("app-info: Add a stub GInitable implementation")
Closes: https://github.com/flatpak/xdg-desktop-portal/issues/1718

src/xdp-app-info-flatpak.c
@@ -825,6 +825,8 @@ xdp_app_info_flatpak_new (int      pid,
                                      "instance", instance,
                                      "pidfd", bwrap_pidfd,
                                      NULL);
+  if (!app_info_flatpak)
+    return NULL;
 
   app_info_flatpak->flatpak_info = g_steal_pointer (&metadata);
 

src/xdp-app-info-test.c
@@ -141,6 +141,7 @@ xdp_app_info_test_new (const char *app_id,
                                   "flags", XDP_APP_INFO_FLAG_HAS_NETWORK |
                                            XDP_APP_INFO_FLAG_SUPPORTS_OPATH,
                                   NULL);
+  g_assert (app_info_test);
 
   app_info_test->usb_queries = parse_usb_queries_string (usb_queries_str);
 