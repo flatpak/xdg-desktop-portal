flatpak/xdg-desktop-portal

commit 2d8c247228e470386aa665594782f1211c97fc98

app-info: Steal pidfd when constructing a XdpAppInfo
This removes a dup which can fail and was also missing a call to mark it
as FD_CLOEXEC. Instead, the XdpAppInfo subclass which manages to
construct, steals the pidfd.

src/xdp-app-info-flatpak.c
@@ -719,7 +719,7 @@ xdp_is_flatpak (int        pid,
 
 XdpAppInfo *
 xdp_app_info_flatpak_new (int      pid,
-                          int      pidfd,
+                          int     *pidfd,
                           GError **error)
 {
   g_autoptr (XdpAppInfoFlatpak) app_info_flatpak = NULL;

src/xdp-app-info-flatpak-private.h
@@ -37,5 +37,5 @@ gboolean xdp_is_flatpak (int        pid,
                          GError   **error);
 
 XdpAppInfo * xdp_app_info_flatpak_new (int      pid,
-                                       int      pidfd,
+                                       int     *pidfd,
                                        GError **error);

src/xdp-app-info-host-private.h
@@ -32,11 +32,11 @@ G_DECLARE_FINAL_TYPE (XdpAppInfoHost,
                       XDP, APP_INFO_HOST,
                       XdpAppInfo)
 
-XdpAppInfo * xdp_app_info_host_new (int pid,
-                                    int pidfd);
+XdpAppInfo * xdp_app_info_host_new (int  pid,
+                                    int *pidfd);
 
 XdpAppInfo *
-xdp_app_info_host_new_registered (int          pidfd,
+xdp_app_info_host_new_registered (int         *pidfd,
                                   const char  *app_id,
                                   GError     **error);
 

src/xdp-app-info-host.c
@@ -186,7 +186,7 @@ get_appid_from_pid (pid_t pid)
 }
 
 XdpAppInfo *
-xdp_app_info_host_new_registered (int          pidfd,
+xdp_app_info_host_new_registered (int         *pidfd,
                                   const char  *app_id,
                                   GError     **error)
 {
@@ -197,7 +197,7 @@ xdp_app_info_host_new_registered (int          pidfd,
                                   error,
                                   "engine", NULL,
                                   "id", app_id,
-                                  "pidfd", pidfd,
+                                  "pidfd", g_steal_fd (pidfd),
                                   "flags", XDP_APP_INFO_FLAG_HAS_NETWORK |
                                            XDP_APP_INFO_FLAG_SUPPORTS_OPATH |
                                            XDP_APP_INFO_FLAG_REQUIRE_GAPPINFO,
@@ -207,8 +207,8 @@ xdp_app_info_host_new_registered (int          pidfd,
 }
 
 XdpAppInfo *
-xdp_app_info_host_new (int pid,
-                       int pidfd)
+xdp_app_info_host_new (int  pid,
+                       int *pidfd)
 {
   g_autoptr(XdpAppInfoHost) app_info_host = NULL;
   g_autofree char *app_id = NULL;
@@ -220,7 +220,7 @@ xdp_app_info_host_new (int pid,
                                   NULL,
                                   "engine", NULL,
                                   "id", app_id,
-                                  "pidfd", pidfd,
+                                  "pidfd", g_steal_fd (pidfd),
                                   "flags", XDP_APP_INFO_FLAG_HAS_NETWORK |
                                            XDP_APP_INFO_FLAG_SUPPORTS_OPATH,
                                   NULL);

src/xdp-app-info-snap-private.h
@@ -37,7 +37,7 @@ gboolean xdp_is_snap (int        pid,
                       GError   **error);
 
 XdpAppInfo * xdp_app_info_snap_new (int      pid,
-                                    int      pidfd,
+                                    int     *pidfd,
                                     GError **error);
 
 XDP_EXPORT_TEST

src/xdp-app-info-snap.c
@@ -253,7 +253,7 @@ xdp_is_snap (int        pid,
 
 XdpAppInfo *
 xdp_app_info_snap_new (int      pid,
-                       int      pidfd,
+                       int     *pidfd,
                        GError **error)
 {
   g_autoptr (XdpAppInfoSnap) app_info_snap = NULL;
@@ -319,7 +319,7 @@ xdp_app_info_snap_new (int      pid,
                                   error,
                                   "engine", "io.snapcraft",
                                   "id", snap_id,
-                                  "pidfd", pidfd,
+                                  "pidfd", g_steal_fd (pidfd),
                                   "flags", flags,
                                   "desktop-file", desktop_id,
                                   NULL);

src/xdp-app-info.c
@@ -224,7 +224,7 @@ xdp_app_info_set_property (GObject      *object,
 
     case PROP_PIDFD:
       g_assert (priv->pidfd == -1);
-      priv->pidfd = dup (g_value_get_int (value));
+      priv->pidfd = g_value_get_int (value);
       break;
 
     default:
@@ -969,7 +969,7 @@ xdp_connection_create_app_info_sync (GDBusConnection  *connection,
 
   if (app_info == NULL)
     {
-      app_info = xdp_app_info_flatpak_new (pid, pidfd, &local_error);
+      app_info = xdp_app_info_flatpak_new (pid, &pidfd, &local_error);
       if (app_info)
         app_info_kind = "flatpak";
     }
@@ -984,7 +984,7 @@ xdp_connection_create_app_info_sync (GDBusConnection  *connection,
 
   if (app_info == NULL)
     {
-      app_info = xdp_app_info_snap_new (pid, pidfd, &local_error);
+      app_info = xdp_app_info_snap_new (pid, &pidfd, &local_error);
       if (app_info)
         app_info_kind = "snap";
     }
@@ -999,7 +999,7 @@ xdp_connection_create_app_info_sync (GDBusConnection  *connection,
 
   if (app_info == NULL)
     {
-      app_info = xdp_app_info_host_new (pid, pidfd);
+      app_info = xdp_app_info_host_new (pid, &pidfd);
       app_info_kind = "derived host";
     }
 
@@ -1054,7 +1054,7 @@ xdp_connection_create_host_app_info_sync (GDBusConnection  *connection,
           return NULL;
         }
 
-      app_info = xdp_app_info_host_new_registered (pidfd, app_id, error);
+      app_info = xdp_app_info_host_new_registered (&pidfd, app_id, error);
       if (!app_info)
         return NULL;
     }

commit f097e47301b7b8f7eb66f9eb1a30a8af3aa6ed9d

app-info/flatpak: Steal bwrap pidfd because AppInfo doesn't dup anymore
Fixes: 2d8c2472 ("app-info: Steal pidfd when constructing a XdpAppInfo")
Closes: https://github.com/flatpak/xdg-desktop-portal/issues/1719

src/xdp-app-info-flatpak.c
@@ -823,7 +823,7 @@ xdp_app_info_flatpak_new (int      pid,
                                      "flags", flags,
                                      "id", id,
                                      "instance", instance,
-                                     "pidfd", bwrap_pidfd,
+                                     "pidfd", g_steal_fd (&bwrap_pidfd),
                                      NULL);
 
   app_info_flatpak->flatpak_info = g_steal_pointer (&metadata);