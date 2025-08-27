flatpak/xdg-desktop-portal

commit 4af976cdeef555d17ee7114c115a2aa5ceb6c3be

app-info: Make use of new XdpAppInfo testing capabilities
Now that the subclasses handle testing requirements themselves, the
generic XdpAppInfo code can stop caring about it, and the XdpAppInfoTest
class can be dropped.

The testing harness is also adjusted to take advantage of this new
feature. In particular, for USB query support the flatpak kind needs to
be used.

This makes it possible to run test with specific or all XdpAppInfo types
that we have.

src/xdp-app-info.c
@@ -46,7 +46,6 @@
 #include "xdp-app-info-flatpak-private.h"
 #include "xdp-app-info-snap-private.h"
 #include "xdp-app-info-host-private.h"
-#include "xdp-app-info-test-private.h"
 #include "xdp-enum-types.h"
 #include "xdp-utils.h"
 
@@ -306,21 +305,6 @@ xdp_app_info_init (XdpAppInfo *app_info)
 }
 
 static XdpAppInfo *
-maybe_create_test_app_info (void)
-{
-  const char *test_override_app_id;
-  const char *test_override_usb_queries;
-
-  test_override_app_id = g_getenv ("XDG_DESKTOP_PORTAL_TEST_APP_ID");
-  if (!test_override_app_id)
-    return NULL;
-
-  test_override_usb_queries = g_getenv ("XDG_DESKTOP_PORTAL_TEST_USB_QUERIES");
-  return xdp_app_info_test_new (test_override_app_id,
-                                test_override_usb_queries);
-}
-
-static XdpAppInfo *
 xdp_app_info_new (uint32_t   pid,
                   int        pidfd,
                   GError   **error)
@@ -329,11 +313,7 @@ xdp_app_info_new (uint32_t   pid,
   g_autofd int pidfd_owned = -1;
   g_autoptr(GError) local_error = NULL;
 
-  app_info = maybe_create_test_app_info ();
-  pidfd_owned = pidfd;
-
-  if (app_info == NULL)
-    app_info = xdp_app_info_flatpak_new (pid, &pidfd_owned, &local_error);
+  app_info = xdp_app_info_flatpak_new (pid, &pidfd_owned, &local_error);
 
   if (!app_info && !g_error_matches (local_error, XDP_APP_INFO_ERROR,
                                      XDP_APP_INFO_ERROR_WRONG_APP_KIND))
@@ -1044,21 +1024,6 @@ xdp_invocation_ensure_app_info_sync (GDBusMethodInvocation  *invocation,
                                               error);
 }
 
-static XdpAppInfo *
-maybe_create_registered_test_app_info (const char *registered_app_id)
-{
-  const char *test_override_app_id;
-  const char *test_override_usb_queries;
-
-  test_override_app_id = g_getenv ("XDG_DESKTOP_PORTAL_TEST_APP_ID");
-  if (!test_override_app_id)
-    return NULL;
-
-  test_override_usb_queries = g_getenv ("XDG_DESKTOP_PORTAL_TEST_USB_QUERIES");
-  return xdp_app_info_test_new (registered_app_id,
-                                test_override_usb_queries);
-}
-
 XdpAppInfo *
 xdp_invocation_register_host_app_info_sync (GDBusMethodInvocation  *invocation,
                                             const char             *app_id,
@@ -1079,29 +1044,24 @@ xdp_invocation_register_host_app_info_sync (GDBusMethodInvocation  *invocation,
       return NULL;
     }
 
-  app_info = maybe_create_registered_test_app_info (app_id);
+  if (!xdp_connection_get_pidfd (connection, sender, cancellable, &pidfd, &pid, error))
+    return NULL;
 
-  if (!app_info)
+  detected_app_info = xdp_app_info_new (pid, pidfd, error);
+  if (!detected_app_info)
+    return NULL;
+
+  if (!xdp_app_info_is_host (detected_app_info))
     {
-      if (!xdp_connection_get_pidfd (connection, sender, cancellable, &pidfd, &pid, error))
-        return NULL;
-
-      detected_app_info = xdp_app_info_new (pid, pidfd, error);
-      if (!detected_app_info)
-        return NULL;
-
-      if (!xdp_app_info_is_host (detected_app_info))
-        {
-          g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
-                       "Can't manually register a %s application",
-                       xdp_app_info_get_engine_display_name (detected_app_info));
-          return NULL;
-        }
-
-      app_info = xdp_app_info_host_new_registered (&pidfd, app_id, error);
-      if (!app_info)
-        return NULL;
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                   "Can't manually register a %s application",
+                   xdp_app_info_get_engine_display_name (detected_app_info));
+      return NULL;
     }
+
+  app_info = xdp_app_info_host_new_registered (&pidfd, app_id, error);
+  if (!app_info)
+    return NULL;
 
   g_debug ("Adding registered host app '%s'", xdp_app_info_get_id (app_info));
 

src/meson.build
@@ -72,7 +72,6 @@ xdp_utils_sources = enums_sources + files(
   'xdp-app-info-host.c',
   'xdp-app-info-flatpak.c',
   'xdp-app-info-snap.c',
-  'xdp-app-info-test.c',
   'xdp-usb-query.c',
 )
 

src/registry.c
@@ -27,7 +27,6 @@
 #include <stdio.h>
 
 #include "xdp-app-info-private.h"
-#include "xdp-app-info-test-private.h"
 #include "xdp-host-dbus.h"
 #include "xdp-utils.h"
 

src/xdp-app-info-test-private.h
@@ -1,36 +1,0 @@
-/*
- * Copyright © 2024 Red Hat, Inc
- *
- * SPDX-License-Identifier: LGPL-2.1-or-later
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>.
- */
-
-#pragma once
-
-#include "xdp-app-info-private.h"
-
-struct _XdpAppInfoTestClass
-{
-  XdpAppInfoClass parent_class;
-};
-
-#define XDP_TYPE_APP_INFO_TEST (xdp_app_info_test_get_type())
-G_DECLARE_FINAL_TYPE (XdpAppInfoTest,
-                      xdp_app_info_test,
-                      XDP, APP_INFO_TEST,
-                      XdpAppInfo)
-
-XdpAppInfo * xdp_app_info_test_new (const char *app_id,
-                                    const char *usb_queries_str);

tests/conftest.py
@@ -4,7 +4,7 @@
 
 import tests.xdp_utils as xdp
 
-from typing import Any, Dict, Iterator, Optional
+from typing import Any, Dict, Iterator, Optional, assert_never
 from types import ModuleType
 
 import pytest
@@ -396,6 +396,15 @@ def xdp_overwrite_env() -> dict[str, str]:
 
 
 @pytest.fixture
+def app_info_kind() -> xdp.AppInfoKind:
+    """
+    Default fixture which can be used to override the XdpAppInfo kind that the
+    portal frontend will discover for incoming connections.
+    """
+    return xdp.AppInfoKind.HOST
+
+
+@pytest.fixture
 def app_id() -> str:
     """
     Default fixture which can be used to override the app id that the portal
@@ -405,21 +414,70 @@ def app_id() -> str:
 
 
 @pytest.fixture
+def usb_queries() -> str | None:
+    """
+    Default fixture providing the usb queries the connecting process can
+    enumerate
+    """
+    return None
+
+
+@pytest.fixture
+def xdp_app_info(app_info_kind, app_id, usb_queries) -> xdp.AppInfo:
+    """
+    Default fixture which can be used to override the XdpAppInfo the portal
+    frontend will discover.
+    This fixture should use the app_id, and usb_queries fixtures
+    to construct the xdp.AppInfo.
+    """
+
+    if app_info_kind == xdp.AppInfoKind.FLATPAK:
+        assert app_id is not None
+
+        return xdp.AppInfo.new_flatpak(
+            app_id=app_id,
+            usb_queries=usb_queries,
+        )
+
+    if app_info_kind == xdp.AppInfoKind.SNAP:
+        assert app_id is not None
+        assert usb_queries is None
+
+        assert app_id.startwith("snap.")
+        snap_name = app_id[5:]
+
+        return xdp.AppInfo.new_snap(
+            snap_name=snap_name,
+        )
+
+    if app_info_kind == xdp.AppInfoKind.HOST:
+        assert app_id is not None
+        assert usb_queries is None
+
+        return xdp.AppInfo.new_host(
+            app_id=app_id,
+        )
+
+    assert_never(app_info_kind)
+
+
+@pytest.fixture(autouse=True)
+def ensure_xdp_app_info_files(create_test_dirs, xdp_app_info) -> None:
+    xdp_app_info.ensure_files()
+
+
+@pytest.fixture
 def xdp_env(
     xdp_overwrite_env: dict[str, str],
-    app_id: str,
-    usb_queries: Optional[str],
+    xdp_app_info: xdp.AppInfo,
     umockdev: Optional[UMockdev.Testbed],
 ) -> dict[str, str]:
     env = os.environ.copy()
     env["G_DEBUG"] = "fatal-criticals"
     env["XDG_CURRENT_DESKTOP"] = "test"
 
-    if app_id:
-        env["XDG_DESKTOP_PORTAL_TEST_APP_ID"] = app_id
-
-    if usb_queries:
-        env["XDG_DESKTOP_PORTAL_TEST_USB_QUERIES"] = usb_queries
+    if xdp_app_info:
+        xdp_app_info.extend_env(env)
 
     if umockdev:
         env["UMOCKDEV_DIR"] = umockdev.get_root_dir()
@@ -562,15 +620,6 @@ def portals(templates: Any, xdg_desktop_portal: Any, xdg_permission_store: Any) 
     """
     Fixture which starts the required templates, xdg-desktop-portal,
     xdg-document-portal and xdg-permission-store. Most tests require this.
-    """
-    return None
-
-
-@pytest.fixture
-def usb_queries() -> Optional[str]:
-    """
-    Default fixture providing the usb queries the connecting process can
-    enumerate
     """
     return None
 

tests/test_registry.py
@@ -6,6 +6,22 @@ import dbus
 import pytest
 
 
+CORRECT_APP_ID_DESKTOP = b"""
+[Desktop Entry]
+Version=1.0
+Name=CorrectAppId
+Exec=true %u
+Type=Application
+"""
+
+
+@pytest.fixture
+def xdg_data_home_files():
+    return {
+        "applications/org.example.CorrectAppId.desktop": CORRECT_APP_ID_DESKTOP,
+    }
+
+
 @pytest.fixture
 def app_id():
     return "org.example.WrongAppId"

tests/test_usb.py
@@ -20,6 +20,11 @@ def required_templates():
 
 
 @pytest.fixture
+def app_info_kind():
+    return xdp.AppInfoKind.FLATPAK
+
+
+@pytest.fixture
 def umockdev():
     return UMockdev.Testbed.new()
 

tests/xdp_utils.py
@@ -8,6 +8,9 @@ from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib, Gio
 from itertools import count
 from typing import Any, Dict, Optional, NamedTuple, Callable, List
+from pathlib import Path
+from enum import Enum
+from dataclasses import dataclass, field
 
 import os
 import dbus
@@ -218,6 +221,193 @@ def check_version(bus: dbus.Bus, portal_name: str, expected_version: int):
     except dbus.exceptions.DBusException as e:
         logger.critical(e)
         assert e is None, str(e)
+
+
+class AppInfoKind(Enum):
+    HOST = 1
+    FLATPAK = 2
+    SNAP = 3
+
+
+@dataclass
+class AppInfo:
+    """
+    Interacts with conftest.py via ensure_files and extend_env to make the
+    portal frontend discover the requested XdpAppInfo for incoming connections.
+
+    Testing code can use this class to construct a specific XdpAppInfo for the
+    xdp_app_info fixture.
+    """
+
+    kind: AppInfoKind
+    app_id: str
+    desktop_file: str
+    env: dict[str, str] = field(default_factory=dict)
+    files: dict[Path, bytes] = field(default_factory=dict)
+
+    def ensure_files(self) -> None:
+        for path, content in self.files.items():
+            path.parent.mkdir(parents=True, exist_ok=True)
+            path.write_bytes(content)
+
+    def extend_env(self, env: dict[str, str]) -> None:
+        for key, val in self.env.items():
+            env[key] = val
+
+    @classmethod
+    def new_host(
+        cls,
+        app_id: str,
+        desktop_entry: bytes | None = None,
+    ):
+        kind = AppInfoKind.HOST
+        desktop_file = f"{app_id}.desktop"
+        env = {
+            "XDG_DESKTOP_PORTAL_TEST_APP_INFO_KIND": "host",
+            "XDG_DESKTOP_PORTAL_TEST_HOST_APPID": app_id,
+        }
+        files = {}
+
+        if desktop_entry:
+            desktop_entry_path = (
+                Path(os.environ["XDG_DATA_HOME"]) / "applications" / f"{app_id}.desktop"
+            )
+            files[desktop_entry_path] = desktop_entry
+
+        return cls(
+            kind=kind,
+            app_id=app_id,
+            desktop_file=desktop_file,
+            env=env,
+            files=files,
+        )
+
+    @classmethod
+    def new_flatpak(
+        cls,
+        app_id: str,
+        instance_id: str | None = None,
+        usb_queries: str | None = None,
+        desktop_entry: bytes | None = None,
+        metadata: bytes | None = None,
+    ):
+        kind = AppInfoKind.FLATPAK
+        desktop_file = f"{app_id}.desktop"
+        env = {
+            "XDG_DESKTOP_PORTAL_TEST_APP_INFO_KIND": "flatpak",
+        }
+        files = {}
+
+        if not instance_id:
+            instance_id = "1234567890"
+
+        if not desktop_entry:
+            desktop_entry = b"""
+[Desktop Entry]
+Version=1.0
+Name=Example App
+Exec=true %u
+Type=Application
+"""
+
+        desktop_entry_path = (
+            Path(os.environ["XDG_DATA_HOME"]) / "applications" / f"{app_id}.desktop"
+        )
+
+        files[desktop_entry_path] = desktop_entry
+
+        if not metadata:
+            metadata_str = f"""
+[Application]
+name={app_id}
+runtime=org.freedesktop.Platform/x86_64/23.08
+sdk=org.freedesktop.Sdk/x86_64/23.08
+command={app_id}
+
+[Instance]
+instance-id={instance_id}
+
+[Context]
+shared=network;ipc;
+sockets=x11;wayland;pulseaudio;fallback-x11;
+devices=dri;
+"""
+            metadata = metadata_str.encode("utf8")
+
+        if usb_queries:
+            metadata_usb_str = f"""
+[USB Devices]
+enumerable-devices={usb_queries}
+"""
+            metadata += metadata_usb_str.encode("utf8")
+
+        metadata_path = Path(os.environ["TMPDIR"]) / "flatpak-metadata"
+
+        files[metadata_path] = metadata
+        env["XDG_DESKTOP_PORTAL_TEST_FLATPAK_METADATA"] = (
+            metadata_path.absolute().as_posix()
+        )
+
+        return cls(
+            kind=kind,
+            app_id=app_id,
+            desktop_file=desktop_file,
+            env=env,
+            files=files,
+        )
+
+    @classmethod
+    def new_snap(
+        cls,
+        snap_name: str,
+        desktop_entry: bytes | None = None,
+        metadata: bytes | None = None,
+    ):
+        kind = AppInfoKind.SNAP
+        app_id = f"snap.{snap_name}"
+        desktop_file = f"desktop-file-{snap_name}.desktop"
+        env = {
+            "XDG_DESKTOP_PORTAL_TEST_APP_INFO_KIND": "snap",
+        }
+        files = {}
+
+        if not desktop_entry:
+            desktop_entry = b"""
+[Desktop Entry]
+Version=1.0
+Name=Example App
+Exec=true %u
+Type=Application
+"""
+
+        desktop_entry_path = (
+            Path(os.environ["XDG_DATA_HOME"]) / "applications" / desktop_file
+        )
+
+        files[desktop_entry_path] = desktop_entry
+
+        if not metadata:
+            metadata_str = f"""
+[Snap Info]
+InstanceName={snap_name}
+DesktopFile={desktop_file}
+"""
+            metadata = metadata_str.encode("UTF-8")
+
+        metadata_path = Path(os.environ["TMPDIR"]) / "snap-metadata"
+
+        files[metadata_path] = metadata
+        env["XDG_DESKTOP_PORTAL_TEST_SNAP_METADATA"] = (
+            metadata_path.absolute().as_posix()
+        )
+
+        return cls(
+            kind=kind,
+            app_id=app_id,
+            desktop_file=desktop_file,
+            env=env,
+            files=files,
+        )
 
 
 class Response(NamedTuple):

commit 5e80cc737b52651c84985abcd685cb4bdcdee967

app-info: Actuall pass in the pidfd we got from dbus
This was accidentally removed when removing surrounding code.

Fixes: 4af976cd ("app-info: Make use of new XdpAppInfo testing capabilities")

src/xdp-app-info.c
@@ -310,7 +310,7 @@ xdp_app_info_new (uint32_t   pid,
                   GError   **error)
 {
   g_autoptr(XdpAppInfo) app_info = NULL;
-  g_autofd int pidfd_owned = -1;
+  g_autofd int pidfd_owned = pidfd;
   g_autoptr(GError) local_error = NULL;
 
   app_info = xdp_app_info_flatpak_new (pid, &pidfd_owned, &local_error);