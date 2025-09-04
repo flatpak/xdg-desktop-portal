956e5d07c4f239e09c9ddb7a2d2e9bfe67640d3e
Sebastian Wick <sebastian.wick@redhat.com>
2025-09-04 17:10:28 +0200

    input-capture: Prepare to hook up clipboard

--- src/input-capture.h
+++ src/input-capture.h
@@ -22,4 +22,26 @@
 
 #include "xdp-types.h"
 
+GType input_capture_session_get_type (void);
+
+typedef struct _InputCaptureSession InputCaptureSession;
+
+G_GNUC_UNUSED static inline InputCaptureSession *
+INPUT_CAPTURE_SESSION (gpointer ptr)
+{
+  return G_TYPE_CHECK_INSTANCE_CAST (ptr, input_capture_session_get_type (), InputCaptureSession);
+}
+
+G_GNUC_UNUSED static inline gboolean
+IS_INPUT_CAPTURE_SESSION (gpointer ptr)
+{
+  return G_TYPE_CHECK_INSTANCE_TYPE (ptr, input_capture_session_get_type ());
+}
+
+gboolean input_capture_session_can_request_clipboard (InputCaptureSession *session);
+
+gboolean input_capture_session_is_clipboard_enabled (InputCaptureSession *session);
+
+void input_capture_session_clipboard_requested (InputCaptureSession *session);
+
 void init_input_capture (XdpContext *context);
