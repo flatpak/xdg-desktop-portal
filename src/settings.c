6ce515a293f418a31c62fdf76b68ab479e8e7807
Sebastian Wick <sebastian.wick@redhat.com>
2025-10-21 17:36:10 +0200

    settings: Get rid of global state

--- src/settings.c
+++ src/settings.c
@@ -41,15 +41,15 @@ typedef struct _SettingsClass SettingsClass;
 struct _Settings
 {
   XdpDbusSettingsSkeleton parent_instance;
+
+  XdpDbusImplSettings **impls;
+  size_t n_impls;
 };
 
 struct _SettingsClass
 {
   XdpDbusSettingsSkeletonClass parent_class;
 };
-
-static XdpDbusImplSettings **impls;
-static int n_impls = 0;
 
 GType settings_get_type (void) G_GNUC_CONST;
 static void settings_iface_init (XdpDbusSettingsIface *iface);
@@ -129,21 +129,24 @@ settings_handle_read_all (XdpDbusSettings       *object,
                           GDBusMethodInvocation *invocation,
                           const char    * const *arg_namespaces)
 {
+  Settings *self = (Settings*)object;
   g_autoptr(GHashTable) merged = NULL;
   g_autoptr(GVariant) settings = NULL;
-  int j;
 
   merged = g_hash_table_new_full (g_str_hash, g_str_equal,
                                   g_free,
                                   (GDestroyNotify) g_variant_dict_unref);
 
-  for (j = n_impls - 1; j >= 0; j--)
+  for (size_t i = 0; i < self->n_impls; i++)
     {
       g_autoptr(GError) error = NULL;
       g_autoptr(GVariant) impl_value = NULL;
+      size_t j = self->n_impls - i - 1;
 
-      if (!xdp_dbus_impl_settings_call_read_all_sync (impls[j], arg_namespaces,
-                                                      &impl_value, NULL, &error))
+      if (!xdp_dbus_impl_settings_call_read_all_sync (self->impls[j],
+                                                      arg_namespaces,
+                                                      &impl_value,
+                                                      NULL, &error))
         g_warning ("Failed to ReadAll() from Settings implementation: %s", error->message);
       else
         merge_impl_settings (merged, impl_value);
@@ -161,17 +164,20 @@ settings_handle_read (XdpDbusSettings       *object,
                       const char            *arg_namespace,
                       const char            *arg_key)
 {
-  int i;
+  Settings *self = (Settings*)object;
 
   g_debug ("Read %s %s", arg_namespace, arg_key);
 
-  for (i = 0; i < n_impls; i++)
+  for (size_t i = 0; i < self->n_impls; i++)
     {
       g_autoptr(GError) error = NULL;
       g_autoptr(GVariant) impl_value = NULL;
 
-      if (!xdp_dbus_impl_settings_call_read_sync (impls[i], arg_namespace,
-                                                  arg_key, &impl_value, NULL, &error))
+      if (!xdp_dbus_impl_settings_call_read_sync (self->impls[i],
+                                                  arg_namespace,
+                                                  arg_key,
+                                                  &impl_value,
+                                                  NULL, &error))
         {
           /* A key not being found is expected, continue to our implementation */
           g_debug ("Failed to Read() from Settings implementation: %s", error->message);
@@ -197,17 +203,20 @@ settings_handle_read_one (XdpDbusSettings       *object,
                           const char            *arg_namespace,
                           const char            *arg_key)
 {
-  int i;
+  Settings *self = (Settings*)object;
 
   g_debug ("ReadOne %s %s", arg_namespace, arg_key);
 
-  for (i = 0; i < n_impls; i++)
+  for (size_t i = 0; i < self->n_impls; i++)
     {
       g_autoptr(GError) error = NULL;
       g_autoptr(GVariant) impl_value = NULL;
 
-      if (!xdp_dbus_impl_settings_call_read_sync (impls[i], arg_namespace,
-                                                  arg_key, &impl_value, NULL, &error))
+      if (!xdp_dbus_impl_settings_call_read_sync (self->impls[i],
+                                                  arg_namespace,
+                                                  arg_key,
+                                                  &impl_value,
+                                                  NULL, &error))
         {
           /* A key not being found is expected, continue to our implementation */
           g_debug ("Failed to Read() from Settings implementation: %s", error->message);
@@ -250,17 +259,20 @@ settings_iface_init (XdpDbusSettingsIface *iface)
 static void
 settings_init (Settings *settings)
 {
-  xdp_dbus_settings_set_version (XDP_DBUS_SETTINGS (settings), 2);
 }
 
 static void
 settings_finalize (GObject *object)
 {
   Settings *self = (Settings*)object;
-  int i;
 
-  for (i = 0; i < n_impls; i++)
-    g_signal_handlers_disconnect_by_data (impls[i], self);
+  for (size_t i = 0; i < self->n_impls; i++)
+    {
+      g_signal_handlers_disconnect_by_data (self->impls[i], self);
+      g_clear_object (&self->impls[i]);
+    }
+
+  g_clear_pointer(&self->impls, g_free);
 
   G_OBJECT_CLASS (settings_parent_class)->finalize (object);
 }
@@ -273,6 +285,28 @@ settings_class_init (SettingsClass *klass)
   object_class->finalize = settings_finalize;
 }
 
+static Settings *
+settings_new (GPtrArray *impls)
+{
+  Settings *settings;
+
+  settings = g_object_new (settings_get_type (), NULL);
+  settings->n_impls = impls->len;
+  settings->impls = (XdpDbusImplSettings **) g_ptr_array_steal (impls, NULL);
+
+  xdp_dbus_settings_set_version (XDP_DBUS_SETTINGS (settings), 2);
+
+  for (size_t i = 0; i < settings->n_impls; i++)
+    {
+      g_signal_connect_object (settings->impls[i], "setting-changed",
+                               G_CALLBACK (on_impl_settings_changed),
+                               settings,
+                               G_CONNECT_DEFAULT);
+    }
+
+  return settings;
+}
+
 void
 init_settings (XdpContext *context)
 {
@@ -280,23 +314,22 @@ init_settings (XdpContext *context)
   GDBusConnection *connection = xdp_context_get_connection (context);
   XdpPortalConfig *config = xdp_context_get_config (context);
   g_autoptr(GPtrArray) impl_configs = NULL;
-  g_autoptr(GError) error = NULL;
+  g_autoptr(GPtrArray) impl_proxies = NULL;
 
-  impl_configs = xdp_portal_config_find_all (config,
-                                             "org.freedesktop.impl.portal.Settings");
+  impl_configs = xdp_portal_config_find_all (config, SETTINGS_DBUS_IMPL_IFACE);
   if (impl_configs->len == 0)
     return;
 
-  impls = g_new (XdpDbusImplSettings *, impl_configs->len);
-
-  settings = g_object_new (settings_get_type (), NULL);
+  impl_proxies = g_ptr_array_new_with_free_func (g_object_unref);
 
   for (size_t i = 0; i < impl_configs->len; i++)
     {
       XdpImplConfig *impl_config = g_ptr_array_index (impl_configs, i);
       const char *dbus_name = impl_config->dbus_name;
+      g_autoptr(XdpDbusImplSettings) impl_proxy = NULL;
+      g_autoptr(GError) error = NULL;
 
-      XdpDbusImplSettings *impl_proxy =
+      impl_proxy =
         xdp_dbus_impl_settings_proxy_new_sync (connection,
                                                G_DBUS_PROXY_FLAGS_NONE,
                                                dbus_name,
@@ -304,20 +337,18 @@ init_settings (XdpContext *context)
                                                NULL,
                                                &error);
       if (impl_proxy == NULL)
-        {
-          g_warning ("Failed to create settings proxy: %s", error->message);
-        }
+        g_warning ("Failed to create settings proxy: %s", error->message);
       else
-        {
-          impls[n_impls++] = impl_proxy;
-          g_signal_connect (impl_proxy, "setting-changed",
-                            G_CALLBACK (on_impl_settings_changed),
-                            settings);
-        }
+        g_ptr_array_add (impl_proxies, g_steal_pointer (&impl_proxy));
     }
 
-  if (n_impls == 0)
-    return;
+  if (impl_proxies->len == 0)
+    {
+      g_warning ("Not providing Settings portal: No working backend");
+      return;
+    }
+
+  settings = settings_new (impl_proxies);
 
   xdp_context_take_and_export_portal (context,
                                       G_DBUS_INTERFACE_SKELETON (g_steal_pointer (&settings)),
