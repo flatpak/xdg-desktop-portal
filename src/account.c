8097c0eef1a2eb261d302c6546893144c1764251
Sebastian Wick <sebastian.wick@redhat.com>
2025-11-05 20:12:16 +0100

    context: Make it possible to dispatch DBus interface skeletons in fibers
    Fibers allow us to handle method calls in a single function which can
    yield by awaiting futures. This means we can get rid of threading
    without blocking the main thread.

--- src/account.c
+++ src/account.c
@@ -308,5 +308,5 @@ init_account (XdpContext *context)
 
   xdp_context_take_and_export_portal (context,
                                       G_DBUS_INTERFACE_SKELETON (g_steal_pointer (&account)),
-                                      XDP_CONTEXT_EXPORT_FLAGS_NONE);
+                                      XDP_CONTEXT_EXPORT_FLAGS_RUN_IN_THREAD);
 }
