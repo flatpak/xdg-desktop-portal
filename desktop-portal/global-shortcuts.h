5b46579340685db0a448194666288602150b5d0d
Sebastian Wick <sebastian.wick@redhat.com>
2025-12-15 22:15:12 +0100

    xdp: Port a few portals to use libdex

--- desktop-portal/global-shortcuts.h
+++ desktop-portal/global-shortcuts.h
@@ -1,9 +1,29 @@
-/* SPDX-License-Identifier: LGPL-2.1-or-later
- * SPDX-FileCopyrightText: Copyright © the xdg-desktop-portal contributors
+/*
+ * Copyright © 2022 Aleix Pol Gonzalez <aleixpol@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *       Aleix Pol Gonzalez <aleixpol@kde.org>
  */
 
 #pragma once
 
+#include <libdex.h>
+
 #include "xdp-types.h"
 
-void init_global_shortcuts (XdpContext *context);
+DexFuture * init_global_shortcuts (gpointer user_data);
