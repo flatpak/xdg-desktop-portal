c3776c2498b5e9449db33223ca35fda6fb4b6d8c
Sebastian Wick <sebastian.wick@redhat.com>
2025-12-15 22:16:04 +0100

    session-dex: Add a new Session implementation which uses futures
    Similar to the Request, this session implementation uses futures and
    doesn't need any external ownership. Unlike a Request, a Session is
    supposed to be used in multiple calls, so the portal needs to store it
    somewhere to look it up from different method handlers. The new
    XdpSessionDexStore can be used for this purpose.

---
+++ desktop-portal/xdp-session-dex.h
@@ -0,0 +1,46 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later
+ * SPDX-FileCopyrightText: Copyright © the xdg-desktop-portal contributors
+ */
+
+#pragma once
+
+#include "xdp-dbus.h"
+#include "xdp-types.h"
+
+#define XDP_TYPE_SESSION_DEX (xdp_session_dex_get_type ())
+G_DECLARE_FINAL_TYPE (XdpSessionDex,
+                      xdp_session_dex,
+                      XDP, SESSION_DEX,
+                      XdpDbusSessionSkeleton)
+
+DexFuture * xdp_session_dex_new (XdpContext             *context,
+                                 XdpAppInfo             *app_info,
+                                 GDBusInterfaceSkeleton *skeleton,
+                                 GDBusProxy             *proxy_impl,
+                                 GVariant               *arg_options);
+
+gboolean xdp_session_dex_is_closed (XdpSessionDex *session);
+
+XdpAppInfo * xdp_session_dex_get_app_info (XdpSessionDex *session);
+
+const char * xdp_session_dex_get_object_path (XdpSessionDex *session);
+
+#define XDP_TYPE_SESSION_DEX_STORE (xdp_session_dex_store_get_type ())
+G_DECLARE_FINAL_TYPE (XdpSessionDexStore,
+                      xdp_session_dex_store,
+                      XDP, SESSION_DEX_STORE,
+                      GObject)
+
+XdpSessionDexStore * xdp_session_dex_store_new (void);
+
+XdpSessionDexStore * xdp_session_dex_store_new_with_offset (size_t session_offset);
+
+#define xdp_session_dex_store_new_wrapped(MySessionWrapper, session) \
+  xdp_session_dex_store_new_with_offset (G_STRUCT_OFFSET (MySessionWrapper, session))
+
+gpointer xdp_session_dex_store_lookup_session (XdpSessionDexStore *store,
+                                               const char         *session_handle,
+                                               XdpAppInfo         *app_info);
+
+void xdp_session_dex_store_take_session (XdpSessionDexStore *store,
+                                         gpointer            session_wrapper);
