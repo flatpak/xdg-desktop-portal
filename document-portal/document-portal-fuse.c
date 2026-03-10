b7d231fa279930b096f8dfbb19541b2fdb1b1650
Gleb Popov <6yearold@gmail.com>
2026-03-10 17:04:19 +0300

    document-portal: Guard renameat2() flags usage behind ifdefs
    FreeBSD recently gained the renameat2 syscall, but only supports
    the RENAME_NOREPLACE flag.

--- document-portal/document-portal-fuse.c
+++ document-portal/document-portal-fuse.c
@@ -428,14 +428,18 @@ renameat2_flags_to_string (int flags)
 #if HAVE_RENAMEAT2
   GString *s = g_string_new ("");
 
+#ifdef RENAME_EXCHANGE
   if (flags & RENAME_EXCHANGE)
     g_string_append (s, "EXCHANGE,");
+#endif
 
   if (flags & RENAME_NOREPLACE)
     g_string_append (s, "NOREPLACE,");
 
+#ifdef RENAME_WHITEOUT
   if (flags & RENAME_WHITEOUT)
     g_string_append (s, "WHITEOUT,");
+#endif
 
   /* Remove last comma */
   if (s->len > 0)
