37a4242d864a373c4f5532b0addc47879402471b
tytan652 <tytan652@tytanium.xyz>
2026-04-11 15:41:56 +0200

    global-shortcuts: Migrate to use revision

--- tests/test_globalshortcuts.py
+++ tests/test_globalshortcuts.py
@@ -15,7 +15,9 @@ def required_templates():
 
 
 class TestGlobalShortcuts:
-    def test_version(self, portals, dbus_con):
+    def test_active_revision(self, portals, dbus_con):
+        xdp.check_active_revision(dbus_con, "GlobalShortcuts", 2)
+        # Check deprecated version to keep it consistent with active revision
         xdp.check_version(dbus_con, "GlobalShortcuts", 2)
 
     def test_create_close_session(self, portals, dbus_con, xdp_app_info):
