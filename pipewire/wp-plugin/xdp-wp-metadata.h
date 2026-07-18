6c1af8cee3d1957d7c73b908a8bcb1ad5c6b7641
tytan652 <tytan652@tytanium.xyz>
2026-07-18 11:50:43 +0200

    pipewire: Introduce metadata for the WirePlumber plugin
    To provide information to xdg-desktop-portal.

---
+++ pipewire/wp-plugin/xdp-wp-metadata.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/* SPDX-FileCopyrightText: Copyright © the xdg-desktop-portal contributors */
+
+#pragma once
+
+#include <gio/gio.h>
+#include <glib-object.h>
+#include <wp/wp.h>
+
+#define XDP_WP_TYPE_METADATA (xdp_wp_metadata_get_type())
+G_DECLARE_FINAL_TYPE (XdpWpMetadata, xdp_wp_metadata, XDP_WP, METADATA, GObject);
+
+void xdp_wp_metadata_new (WpCore              *core,
+                          GCancellable        *cancellable,
+                          GAsyncReadyCallback  callback,
+                          gpointer             user_data);
+
+XdpWpMetadata * xdp_wp_metadata_new_finish (GObject       *object,
+                                            GAsyncResult  *res,
+                                            GError       **error);
